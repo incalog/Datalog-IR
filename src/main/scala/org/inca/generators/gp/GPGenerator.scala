e6174d39e2b686608f82746bcaefd6b9d657704d
paul <paul.hempel@ilume.de>
2019-12-17 14:11:38 +0100

    added temporary variables from graph

--- src/main/scala/org/inca/generators/gp/GPGenerator.scala
+++ src/main/scala/org/inca/generators/gp/GPGenerator.scala
@@ -1,17 +1,26 @@
 package org.inca.generators.gp
 
+import org.inca.core.Content.TemporaryVariable
+import org.inca.gp.Constraints.PatternCompositionConstraint
 import org.inca.gp.Content.GraphPattern
 
 import scala.meta._
 
+
 class GPGenerator {
   val filename = "test.scala"
 
+  /**
+   *
+   * @param pattern GraphPattern which will be transformed into scalameta tree
+   * @param collectionName Name of the file (mps) where pattern is saved
+   * @return
+   */
   def generate(pattern: GraphPattern, collectionName: String): Defn.Class = {
 
-
-    val fullyQualifiedName = s"""$collectionName.${pattern.name}"""
-    // todo maybe refactor
+    /*
+     * !!! heads up: tree is created bottom-up !!!
+     */
 
 
     /*
@@ -34,17 +43,42 @@ class GPGenerator {
      */
     var doGetContainedBodies_bodies: List[Stat] = List()
 
+    // create GraphPattern Bodies
     for (body <- pattern.bodies) {
       var bodyList: List[Stat] = List()
       val PBody_body = q"val body: PBody = PBody(this)"
       bodyList = bodyList ++ List(PBody_body)
 
 
+      // create local global variable
       for (param <- pattern.parameters) {
-        val paramName =Lit.String(param.name)
+        val paramName = Lit.String(param.name)
         val param_var_name = Pat.Var(Term.Name(s"var_${param.name}"))
 
         val pVariable_var_list_entry = q"val $param_var_name: PVariable = body.getOrCreateVariableByName($paramName)"
+
+        bodyList = bodyList ++ List(pVariable_var_list_entry)
+      }
+
+
+      var tempVars: List[String] = List()
+
+      // find temporary variables in GraphPattern Bodies
+      body.contents.foreach {
+        case p: PatternCompositionConstraint =>
+          p.call.arguments.foreach {
+            case t: TemporaryVariable =>
+              tempVars = tempVars ++ List(t.name)
+            case _ => null
+          }
+        case _ => null
+      }
+
+      for(tempVar <- tempVars) {
+        val tempVarValue = Lit.String(tempVar)
+        val tempVarName = Pat.Var(Term.Name(s"var__${tempVar}"))
+
+        val pVariable_var_list_entry = q"val $tempVarName: PVariable = body.getOrCreateVariableByName($tempVarValue)"
 
         bodyList = bodyList ++ List(pVariable_var_list_entry)
       }
@@ -79,6 +113,7 @@ class GPGenerator {
 
     println(rootClass)
 
+    // return ast
     rootClass
   }
 }
