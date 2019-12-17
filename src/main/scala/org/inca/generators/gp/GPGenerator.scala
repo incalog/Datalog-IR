ef291852f66819a0de9be3f6847dd24d75baa85e
paul <paul.hempel@ilume.de>
2019-12-17 13:58:30 +0100

    generator progress

--- src/main/scala/org/inca/generators/gp/GPGenerator.scala
+++ src/main/scala/org/inca/generators/gp/GPGenerator.scala
@@ -1,67 +1,84 @@
 package org.inca.generators.gp
 
 import org.inca.gp.Content.GraphPattern
+
 import scala.meta._
 
 class GPGenerator {
   val filename = "test.scala"
 
-  def generate(pattern: GraphPattern, collectionName: String): Unit = {
+  def generate(pattern: GraphPattern, collectionName: String): Defn.Class = {
+
+
     val fullyQualifiedName = s"""$collectionName.${pattern.name}"""
-    val className = s"""${pattern.name}_${collectionName}QuerySpecification"""
-    var program =
-      s"""
-      class ${className} extends ScalaQuerySpecification {
+    // todo maybe refactor
 
-        class GeneratedPQuery extends AbstractPQuery {
-      """
 
+    /*
+     * GeneratedPQuery -> PParameter
+     */
+    var generatedPQuery_body: List[Stat] = List()
     for (param <- pattern.parameters) {
-      program += // todo FTR append ConceptKey()
-        s"""
-          val p_${param.name}: PParameter = PParameter("${param.name}", "${param.typ.get.toString.substring(1)}", ConceptKey())"""
+      val paramName = Pat.Var(Term.Name(s"p_${param.name}"))
+      val paramNameString = Lit.String(s"p_${param.name}")
+      val paramFullyQualifiedName = Lit.String(param.typ.get.toString.substring(1))
+      val paramConceptKey = q"ConceptKey()"
+      val paramValue = q"PParameter($paramNameString, $paramFullyQualifiedName, $paramConceptKey)"
+      val parVal = q"val $paramName: PParameter = $paramValue"
+
+      generatedPQuery_body = generatedPQuery_body ++ List(parVal)
     }
 
-    program +=
-      s"""
-
-          val INSTANCE: ${className}.GeneratedPQuery = ${className}.GeneratedPQuery()"""
-
-    program +=
-      s"""
-
-          def doGetContainedBodies(): Set[PBody] = {"""
+    /*
+     * GeneratedPQuery -> doGetContainedBodies
+     */
+    var doGetContainedBodies_bodies: List[Stat] = List()
 
     for (body <- pattern.bodies) {
-      program +=
-        s"""
-            {
-              var body = PBody(this) """
+      var bodyList: List[Stat] = List()
+      val PBody_body = q"val body: PBody = PBody(this)"
+      bodyList = bodyList ++ List(PBody_body)
+
 
       for (param <- pattern.parameters) {
-        program +=
-          s"""
-              val var_${param.name}: PVariable = body.getOrCreateVariableByName("${param.name}")"""
+        val paramName =Lit.String(param.name)
+        val param_var_name = Pat.Var(Term.Name(s"var_${param.name}"))
+
+        val pVariable_var_list_entry = q"val $param_var_name: PVariable = body.getOrCreateVariableByName($paramName)"
+
+        bodyList = bodyList ++ List(pVariable_var_list_entry)
       }
-      program +=
-        s"""
-            }"""
+
+      val body_content = q"{ ..$bodyList }"
+      doGetContainedBodies_bodies = doGetContainedBodies_bodies ++ List{body_content}
     }
 
-    program +=
-      s"""
-          }
-        }
+    val doGetContainedBodies_body = q"{..$doGetContainedBodies_bodies}"
 
-        def getFullyQualifiedName(): String = "$fullyQualifiedName"
-      }"""
-    val tree = program.parse[Source].get
+    val doGetContainedBodies = q"override def doGetContainedBodies(): Set[PBody] = $doGetContainedBodies_body"
 
-    println(tree)
-//    println(tree.structure)
-  }
+    generatedPQuery_body = generatedPQuery_body ++ List(doGetContainedBodies)
 
-  def printFl(text: String): Unit = {
+    /*
+     * GeneratedPQuery -> INSTANCE Variable
+     */
 
+    //    val instanceValName = Type.Name(s"${pattern.name}_${collectionName}QuerySpecification.GeneratedPQuery")
+    //    val instanceVal = q"val INSTANCE: $instanceValName = ${className}.GeneratedPQuery()"
+
+    /*
+     * GeneratedPQuery
+     */
+    val innerClass = q"class GeneratedPQuery extends AbstractPQuery { ..$generatedPQuery_body }"
+
+    /*
+     * class
+     */
+    val className = Type.Name(s"${pattern.name}_${collectionName}QuerySpecification")
+    val rootClass = q"class $className extends ScalaQuerySpecification { $innerClass }"
+
+    println(rootClass)
+
+    rootClass
   }
 }
