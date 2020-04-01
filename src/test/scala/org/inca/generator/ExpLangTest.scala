d1837a86f0d1b1e662fde2d62ee8726a4c97d663
paul <paul.hempel@ilume.de>
2020-04-01 13:13:34 +0200

    tests for concept and composition constraints with the ExpLang

--- src/test/scala/org/inca/generator/ExpLangTest.scala
+++ src/test/scala/org/inca/generator/ExpLangTest.scala
@@ -4,7 +4,7 @@ import org.eclipse.viatra.query.runtime.rete.matcher.DifferentialReteBackendFact
 import org.inca.analyzedLangs._
 import org.inca.gen.Pipeline.generateGraphPattern
 import org.inca.incer.indices.{EnginePool, TFQueryScope}
-import org.inca.lang.Core.CoreVariableReference
+import org.inca.lang.Core.{CoreVariableReference, PatternCall}
 import org.inca.lang.Gp._
 import org.inca.meta.MetaElements.NodeType
 import org.scalatest.funsuite.AnyFunSuite
@@ -15,15 +15,15 @@ class ExpLangTest extends AnyFunSuite {
   private val expType = NodeType(classOf[Expression])
   private val intType = NodeType(classOf[IntegerLit])
   private val longType = NodeType(classOf[LongLit])
-  //  private val boolType = NodeType(classOf[BooleanLit])
+  private val boolType = NodeType(classOf[BooleanLit])
 
   private val expGPP = GraphPatternParameter("exp", Some(expType))
 
   /**
    * pattern Number(exp : Expression) {
-   *   IntegerLit(exp)
+   *  IntegerLit(exp)
    * } or {
-   *   LongLit(exp)
+   *  LongLit(exp)
    * }
    */
   private val numberPattern: GraphPattern = GraphPattern(
@@ -46,14 +46,87 @@ class ExpLangTest extends AnyFunSuite {
     None
   )
 
+  /**
+   * pattern Boolean(exp : Expression) {
+   *  IntegerLit(exp)
+   * } or {
+   *  LongLit(exp)
+   * }
+   */
+  private val booleanPattern: GraphPattern = GraphPattern(
+    "Boolean",
+    Seq(
+      expGPP
+    ),
+    Seq(
+      GraphPatternBody(
+        Seq(
+          ConceptConstraint(CoreVariableReference(expGPP), boolType)
+        )
+      )
+    ),
+    None
+  )
+
+  /**
+   * pattern Primitives(exp : Expression) {
+   *  find Number(exp)
+   * } or {
+   *  find Boolean(exp)
+   * } // or String or ...
+   */
+  private val primitivesPattern: GraphPattern = GraphPattern(
+    "Primitives",
+    Seq(
+      expGPP
+    ),
+    Seq(
+      GraphPatternBody(
+        Seq(
+          CompositionConstraint(neg = false, PatternCall(transitive = false, Seq(CoreVariableReference(expGPP)), numberPattern))
+        )
+      ),
+      GraphPatternBody(
+        Seq(
+          CompositionConstraint(neg = false, PatternCall(transitive = false, Seq(CoreVariableReference(expGPP)), booleanPattern))
+        )
+      )
+    ),
+    None
+  )
 
   private val testInput = Add(And(Or(BooleanLit(true), BooleanLit(false)), IntegerLit(5)), LongLit(10))
 
-  test("Test `Number` pattern") {
+  /**
+   * Do this first, if you have no pattern file, else you can skip
+   */
+  test("Generate patterns") {
     writeClass(numberPattern)
+    writeClass(booleanPattern)
+    writeClass(primitivesPattern)
+  }
+
+  test("Test concept and composition constraint") {
+    // updates generated files
+    writeClass(numberPattern)
+    writeClass(booleanPattern)
+    writeClass(primitivesPattern)
 
     val scope = new TFQueryScope(testInput)
-    val matcher = EnginePool.getMatcher(generated.Number.instance(), scope, DifferentialReteBackendFactory.INSTANCE)
-    println(matcher.getAllMatches)
+
+    val numberMatcher = EnginePool.getMatcher(generated.Number.instance(), scope, DifferentialReteBackendFactory.INSTANCE)
+    val primitivesMatcher = EnginePool.getMatcher(generated.Primitives.instance(), scope, DifferentialReteBackendFactory.INSTANCE)
+
+    println("Concept Constraint Matches:")
+    // should contain both, the LongLit with value 10 and the IntegerLit with value 5
+    println(numberMatcher.getAllMatches)
+    assert(!numberMatcher.getAllMatches.isEmpty)
+
+    println("Composition Constraint Matches:")
+    // should contain the previous values and
+    // the two BooleanLit's with false and true als values
+    println(primitivesMatcher.getAllMatches)
+    assert(!primitivesMatcher.getAllMatches.isEmpty)
+
   }
 }
