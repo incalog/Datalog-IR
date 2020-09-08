531a77f06c85dbcad1a664345d00d8022c512b53
Ronja Schnur <rschnur@students.uni-mainz.de>
2020-09-08 09:00:12 +0200

    :sparkles: Added interface / trait for extention parsers.

---
+++ src/main/scala/inca/frontend/parser/ExpressionParserExtention.scala
@@ -0,0 +1,38 @@
+package inca.frontend.parser
+
+import fastparse._
+import NoWhitespace._ 
+import inca.frontend.core.Core._
+
+trait ExpressionParserExtention
+{
+    /** This function should return a Sequence of expression parsers,
+      * which don't require left hand recursion.
+      * 
+      * An example would be: def parser[_:P]: P[Exp] = P(ConstantParser ~ ...)
+      * The constant parser is a example parser not calling any recursion on exp.
+      * 
+      * @return Sequence of parsers with type P[Exp]
+      */
+    def anchorExpressions : Seq[P[Exp]] = Seq.empty
+
+    /**
+      * This function should return a sequence of expressions parsers that would
+      * require left hand recursion. 
+      * Therefore these parsers must accept a @see Exp as function argument.
+      * 
+      * An example would be: def parser[_:P](e: Exp) : P[Exp] = P(ConstantParser ~ ...)
+      * The constant parser is a example parser not calling any recursion on exp.
+      *
+      * @return Sequence of parser functions with type Exp => P[Exp]
+      */
+    def recursiveExpressions : Seq[Exp => P[Exp]] = Seq.empty
+
+
+    /**
+      * This function should return a sequence of statement parsers.
+      *
+      * @return Sequence of parser functions with type P[Statement]
+      */
+    def statement : Seq[P[Statement]] = Seq.empty
+}
