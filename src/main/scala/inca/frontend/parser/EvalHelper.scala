f9245087efa5fa7225bb5b3e87f20b69c2085210
fluffyChaos <julechor@gmail.com>
2020-09-10 16:24:40 +0200

    most terms implemented. Added support for more patterns

--- src/main/scala/inca/frontend/parser/EvalHelper.scala
+++ src/main/scala/inca/frontend/parser/EvalHelper.scala
@@ -1,7 +1,7 @@
 package inca.frontend.parser
 
 import scala.collection.mutable
-import scala.meta.{Case, Defn, Pat, Term, Tree}
+import scala.meta.{Case, Defn, Enumerator, Lit, Pat, Term, Tree}
 import inca.frontend.core.Core.Name
 
 object EvalHelper {
@@ -9,10 +9,13 @@ object EvalHelper {
   /**
    * Computes the set of free(unbound) variables in this AST
    * @param term the AST
+   * @param initBound an optional initial set of bound variables
    * @return the set of free variables
    */
-  def freeVars(term: Tree): scala.collection.immutable.Set[Name] = {
-    loadVars(term, new Scope(mutable.Set(), mutable.Set())).free.toSet
+  def freeVars(term: Tree, initBound: Set[String] = Set()): scala.collection.immutable.Set[Name] = {
+    val scope = new Scope(mutable.Set(), initBound.to(mutable.Set))
+    loadVars(term, scope)
+    scope.free.toSet
   }
 
   /**
@@ -20,9 +23,12 @@ object EvalHelper {
    * into the scope
    * @param term the AST
    * @param scope the scope of the AST node into which the variables are loaded
-   * @return
    */
-  private def loadVars(term: Tree, scope: Scope): Scope = term match {
+  private def loadVars(term: Tree, scope: Scope): Unit = term match {
+
+    //literals don't contain variables
+    case _: Lit =>
+
     // definitions
     case Defn.Val(_, pats, _, rhs) =>
       val patVars = extractVars(pats, scope)
@@ -35,16 +41,17 @@ object EvalHelper {
       patVars.foreach(scope.newBound)
       if(rhs.isDefined) {
         loadVars(rhs.get, scope)
-      } else {
-        scope
       }
+    case _: Defn.Macro
+         | _: Defn.Class
+         | _: Defn.Object
+         | _: Defn.Def
+         | _: Defn.Trait
+         | _: Defn.Type => throw new UnsupportedOperationException("only val and var are supported definitions")
 
     // terms
     case Term.Name(name) =>
-      if(scope.isBound(name) || scope.isFree(name)) {
-        //we have already encountered this variable
-        scope
-      } else {
+      if(!scope.isBound(name) && !scope.isFree(name)) {
         // variable not already known => must be free
         scope.newFree(name)
       }
@@ -53,52 +60,61 @@ object EvalHelper {
     case Term.Select(qual, _) => loadVars(qual, scope)
     case Term.ApplyUnary(_, arg) => loadVars(arg, scope)
     case Term.Return(expr) => loadVars(expr, scope)
-    case Term.Annotate(expr, _) => loadVars(expr, scope)
+    case Term.Annotate(expr, annots) =>
+      loadVars(expr, scope)
+      annots.foreach(_.init.argss.foreach(loadAllVars(_, scope)))
     case Term.Ascribe(expr, _) => loadVars(expr, scope)
     case Term.Throw(expr) => loadVars(expr, scope)
     case Term.Repeated(expr) => loadVars(expr, scope)
 
-    case Term.Interpolate(_, _, args) => foldFreeVars(args, scope)
-    case Term.Tuple(args) => foldFreeVars(args, scope)
+    case Term.Interpolate(prefix, _, args) =>
+      loadAllVars(args, scope)
+      if(!scope.isBound(prefix.value)) {
+        scope.newFree(prefix.value)
+      }
+    case Term.Tuple(args) => loadAllVars(args, scope)
 
     case Term.Block(stats) =>
       // a block defines a new scope nested in the current one
-      foldFreeVars(stats, scope.nestedScope())
-      scope
+      loadAllVars(stats, scope.nestedScope())
 
-    case Term.ApplyInfix(lhs, _, _, args) => foldFreeVars(args, loadVars(lhs, scope))
-    case Term.Apply(fun, args) => foldFreeVars(args, loadVars(fun, scope))
+    case Term.ApplyInfix(lhs, _, _, args) =>
+      loadVars(lhs, scope)
+      loadAllVars(args, scope)
+    case Term.Apply(fun, args) =>
+      loadVars(fun, scope)
+      loadAllVars(args, scope)
 
     case Term.Function(params, body) =>
-      params.foreach(p => scope.newBound(p.name.value))
-      loadVars(body, scope)
+      val scope1 = scope.nestedScope()
+      params.foreach(p => scope1.newBound(p.name.value))
+      loadVars(body, scope1)
 
     case Term.Assign(lhs, rhs) =>
-      loadVars(rhs, loadVars(lhs, scope))
+      loadVars(lhs, scope)
+      loadVars(rhs, scope)
 
     case Term.Match(expr, cases) =>
-      val scope1 = loadVars(expr, scope)
-      cases.foreach(c => fromCase(c, scope1))
-      scope1
+      loadVars(expr, scope)
+      cases.foreach(c => loadFromCase(c, scope))
 
     case Term.If(cond, thenp, elsep) =>
-      loadVars(elsep, loadVars(thenp, loadVars(cond, scope)))
+      loadVars(cond, scope)
+      loadVars(thenp, scope)
+      loadVars(elsep, scope)
 
     case Term.New(init) =>
-      init.argss.foreach(foldFreeVars(_, scope))
-      scope
+      init.argss.foreach(loadAllVars(_, scope))
 
     case Term.PartialFunction(cases) =>
-      cases.foreach(c => fromCase(c, scope))
-      scope
+      cases.foreach(c => loadFromCase(c, scope))
 
     case Term.Try(expr, catchp, finallyp) =>
       loadVars(expr, scope)
-      catchp.foreach(c => fromCase(c, scope))
+      catchp.foreach(c => loadFromCase(c, scope))
       if(finallyp.isDefined) {
         loadVars(finallyp.get, scope)
       }
-      scope
 
     case Term.TryWithHandler(expr, catchp, finallyp) =>
       loadVars(expr, scope)
@@ -106,14 +122,8 @@ object EvalHelper {
       if(finallyp.isDefined){
         loadVars(finallyp.get, scope)
       }
-      scope
 
-    case Term.NewAnonymous(templ) =>
-      val nested = scope.nestedScope()
-      foldFreeVars(templ.early, nested)
-      foldFreeVars(templ.stats, nested)
-      templ.inits.foreach(init => init.argss.foreach(foldFreeVars(_, nested)))
-      scope
+    case Term.NewAnonymous(_) => throw new UnsupportedOperationException("NewAnonymous currently not supported")
 
     case Term.Do(body, expr) =>
       loadVars(body, scope)
@@ -121,28 +131,48 @@ object EvalHelper {
 
     case Term.For(enums, body) =>
       val nested = scope.nestedScope()
-      foldFreeVars(enums, nested)
+      enums.foreach(loadFromEnumerator(_, nested))
       loadVars(body, nested)
 
     case Term.ForYield(enums, body) =>
       val nested = scope.nestedScope()
-      foldFreeVars(enums, nested)
+      enums.foreach(loadFromEnumerator(_, nested))
       loadVars(body, nested)
 
     case Term.While(expr, body) =>
       loadVars(expr, scope)
       loadVars(body, scope)
 
-    case _ => scope
+    case Term.Xml(_, args) => loadAllVars(args, scope)
+
+    case Term.Super(_, _)
+         | Term.This(_) =>
+
+    case ex => throw new UnsupportedOperationException(s"not yet implemented: $ex")
   }
 
-  private def foldFreeVars(terms: List[Tree], scope: Scope): Scope = {
+  private def loadAllVars(terms: List[Tree], scope: Scope): Unit = {
     terms.foreach(t => loadVars(t, scope))
-    scope
   }
 
   private def extractVars(pats: List[Pat], scope: Scope): mutable.Set[Name] = {
     pats.flatMap(definedVars(_, scope)).to(mutable.Set)
+  }
+
+  private def loadFromEnumerator(enum: Enumerator, scope: Scope): Unit = {
+    enum match {
+      case Enumerator.Generator(pat, rhs) =>
+        val patVars = definedVars(pat, scope)
+        loadPatVars(patVars, scope)
+        loadVars(rhs, scope)
+      case Enumerator.Val(pat, rhs) =>
+        val patVars = definedVars(pat, scope)
+        patVars.foreach(scope.newBound)
+        loadVars(rhs, scope)
+      case Enumerator.Guard(cond) =>
+        loadVars(cond, scope)
+      case Enumerator.Quasi(_, _) => throw new UnsupportedOperationException("Enumerator.Quasi not currently supported")
+    }
   }
 
   /**
@@ -151,6 +181,7 @@ object EvalHelper {
    * @return the set of free variables in the pattern
    */
   private def definedVars(pat: Pat, scope: Scope): mutable.Set[Name] = pat match {
+    case _: Lit => mutable.Set()
     case Pat.Var(Term.Name(name)) => mutable.Set(name)
     case Pat.Bind(lhs, rhs) => definedVars(lhs, scope) ++ definedVars(rhs, scope)
     case Pat.Tuple(args) => args.foldLeft(mutable.Set[Name]()) {
@@ -161,31 +192,69 @@ object EvalHelper {
       loadVars(fun, scope)
       extractVars(args, scope)
     case Pat.Typed(p, _) => definedVars(p, scope)
+    case Pat.Wildcard() => mutable.Set()
+    case Pat.SeqWildcard() => mutable.Set()
+    case Pat.Quasi(_, _) => throw new UnsupportedOperationException("Pat.Quasi is currently not supported")
+    case Pat.Xml(_, _) => throw new UnsupportedOperationException("Pat.XML is not supported")
+    case Term.Select(Term.Name(name), _) =>
+      mutable.Set(name)
+    case _ => throw new UnsupportedOperationException(s"not yet implemented: $pat")
   }
 
-  private def fromCase(cas: Case, scope: Scope): Unit = {
+  private def loadPatVars(vars: mutable.Set[String], scope: Scope): Unit = {
+    vars.foreach(
+      v =>
+        if(v.charAt(0).isUpper && !scope.isBound(v)) {
+          // pattern variables with the first char in upper case are assumed to be constants defined somewhere else
+          scope.newFree(v)
+        }
+        else {
+          scope.newBound(v)
+        }
+    )
+  }
+
+  private def loadFromCase(cas: Case, scope: Scope): Unit = {
     val patVars = definedVars(cas.pat, scope)
+    patVars.foreach(scope.newBound)
     if(cas.cond.isDefined) {
-      patVars.foreach(scope.newBound)
-      loadVars(cas.cond.get, scope).free
+      loadVars(cas.cond.get, scope)
     }
     loadVars(cas.body, scope.nestedScope())
   }
 
+  /**
+   * A scope represents the free and bound variables in a specific code block.
+   * Blocks for example create a new scope nested in the current one
+   * so variables newly bound in this nested scope are only bound there and not in it's parent scope.
+   * For the purpose of finding all free variables in an given AST this implementation uses only one set of free vars.
+   * This means once an initial scope for the root of the tree is created, it and all it's nested scopes share this set
+   * while the set of bound variables is cloned each time a new nested scope is created
+   * @param free the set of free variables known in this scope
+   * @param bound the set of bound variables known in this scope
+   */
   private class Scope(val free: mutable.Set[Name], val bound: mutable.Set[Name]) {
 
     def isFree(v: Name): Boolean = free(v)
 
     def isBound(v: Name): Boolean = bound(v)
 
-    def newFree(f: Name): Scope = {
-      free += f
-      this
+    /**
+     * Adds the name to the set of known free variables.
+     * Does nothing if the name is already referencing a known free variable
+     * @param name the name of the new free variable
+     */
+    def newFree(name: Name): Unit = {
+      free += name
     }
 
-    def newBound(b: Name): Scope = {
-      bound += b
-      this
+    /**
+     * Adds the name to the set of known bound variables.
+     * Does nothing if the name is already referencing a known bound variable
+     * @param name the name of the new bound variable
+     */
+    def newBound(name: Name): Unit = {
+      bound += name
     }
 
     def nestedScope(): Scope = {
--- src/test/scala/inca/frontend/parser/EvalHelperTest.scala
+++ src/test/scala/inca/frontend/parser/EvalHelperTest.scala
@@ -1,51 +1,390 @@
 package inca.frontend.parser
 
-import inca.frontend.core.Core.Name
+import inca.frontend.core.Core.{Continue, Name}
 import org.scalatest.funsuite.AnyFunSuite
 
-import scala.meta.{Term, XtensionParseInputLike}
+import scala.meta.Term._
+import scala.meta.{Case, Defn, Enumerator, Init, Lit, Mod, Pat, Term, Tree, Type, XtensionParseInputLike}
 
 class EvalHelperTest extends AnyFunSuite {
 
+  private val tInt = Type.Name("Int")
+
+  private val defValue = Defn.Val(Nil, List(Pat.Var(Name("value"))), None, Name("free"))
+  private val defVar = Defn.Var(Nil, List(Pat.Var(Name("value"))), None, Some(Name("free")))
+  private val undefinedVar = Defn.Var(Nil, List(Pat.Var(Name("value"))),Some(Type.Name("Int")), None)
+
+  private val paramN = Param(Nil, Name("n"), Some(tInt), None)
+
   test("test freeVars Name") {
-    checkVars("x", Set("x"))
+    checkVars(Term.Name("x"), Set("x"))
   }
 
   test("test freeVars select") {
-    checkVars("x.name", Set("x"))
+    checkVars(Term.Select(Name("x"), Name("prop")), Set("x"))
   }
 
   test("test freeVars apply unary") {
-    checkVars("!ten", Set("ten"))
+    checkVars(ApplyUnary(Name("!"), Name("ten")), Set("ten"))
   }
 
   test("test freeVars apply infix") {
-    checkVars("x + y", Set("x", "y"))
+    checkVars(ApplyInfix(Name("x"), Name("-"), Nil, List(Name("y"))), Set("x", "y"))
   }
 
   test("test freeVars return") {
-    checkVars("return value", Set("value"))
+    checkVars(Return(Name("value")), Set("value"))
   }
 
   test("test freeVars ascribe") {
-    checkVars("value.asInstanceOf[Int]", Set("value"))
+    checkVars(Ascribe(Name("value"), tInt), Set("value"))
   }
 
   test("test freeVars throw") {
-    checkVars("throw value", Set("value"))
+    checkVars(Throw(Name("value")), Set("value"))
   }
 
   test("test freeVars apply") {
-    checkVars("fun(arg)", Set("fun", "arg"))
+    checkVars(Apply(Name("fun"), List(Name("arg"))), Set("fun", "arg"))
   }
 
   test("test freeVars tuple") {
-    checkVars("(val1, val2, val3)", Set("val1", "val2", "val3"))
+    checkVars(Tuple(List(Name("val1"), Name("val2"), Name("val3"))), Set("val1", "val2", "val3"))
   }
 
-  private def checkVars(code: String, expectedFree: Set[Name]): Unit = {
-    val tree = code.parse[Term].get
-    val free = EvalHelper.freeVars(tree).toSet
+  test("test freeVars definition val") {
+    checkVars(defValue, Set("free"))
+  }
+
+  test("test freeVars definition var no rhs") {
+    checkVars(undefinedVar, Set())
+  }
+
+  test("test freeVars definition var with rhs") {
+    checkVars(defVar, Set("free"))
+  }
+
+  test("test freeVars function") {
+    checkVars(Function(List(paramN), Name("n")), Set())
+  }
+
+  test("test freeVars assign free var") {
+    val code = Assign(Name("free"), Name("free1"))
+    checkVars(code, Set("free", "free1"))
+  }
+
+  test("test freeVars block single global block") {
+    val code = Block(
+      List(
+        Name("stat1"),
+        Name("stat2")
+      )
+    )
+    checkVars(code, Set("stat1", "stat2"))
+  }
+
+  test("test freeVars block code after local block") {
+    val code = Block(
+      List(
+        Block(
+          List(
+            Name("free1"),
+            defValue
+          )
+        ),
+        Name("value")
+      )
+    )
+    checkVars(code, Set("free1", "free", "value"))
+  }
+
+  test("test freeVars block nested blocks") {
+    val code = Block(
+      List(
+        defValue,
+        Block(
+          List(
+            defVar,
+            Name("value")
+          )
+        ),
+        Name("value")
+      )
+    )
+    checkVars(code, Set("free"))
+  }
+
+  test("test freeVars assign bound var") {
+    val code = Block(
+      List(
+        undefinedVar,
+        Assign(Name("value"), Name("free"))
+      )
+    )
+    checkVars(code, Set("free"))
+  }
+
+  test("test freeVars if") {
+    val code = If(Name("cond"), Name("free"), Name("free1"))
+    checkVars(code, Set("cond", "free", "free1"))
+  }
+
+  test("test freeVars try no catch no finally") {
+    val code = Try(
+      Name("free"),
+      Nil,
+      None
+    )
+    checkVars(code, Set("free"))
+  }
+
+  test("test freeVars try one catch no finally") {
+    val code = Try(
+      Name("free"),
+      List(
+        Case(
+          Pat.Var(Name("e")),
+          None,
+          Name("e")
+        )
+      ),
+      None
+    )
+    checkVars(code, Set("free"))
+  }
+
+  test("test freeVars try no catch finally") {
+    val code = Try(
+      Name("free"),
+      Nil,
+      Some(
+        Name("free1")
+      )
+    )
+    checkVars(code, Set("free", "free1"))
+  }
+
+  test("test freeVars try multiple catch finally") {
+    val code = Try(
+      Name("free"),
+      List(
+        Case(
+          Pat.Var(Name("e1")),
+          None,
+          Name("e2")
+        ),
+        Case(
+          Pat.Var(Name("e")),
+          Some(Name("cond")),
+          Name("e")
+        )
+      ),
+      Some(
+        Name("free1")
+      )
+    )
+    checkVars(code, Set("free", "free1", "e2", "cond"))
+  }
+
+  test("test freeVars try with handler") {
+    val code = TryWithHandler(
+      Name("free"),
+      Name("e"),
+      None
+    )
+    checkVars(code, Set("free", "e"))
+  }
+
+  test("test freeVars for") {
+    val code = For(
+      List(
+        Enumerator.Generator(Pat.Var(Name("x")), Name("xs")),
+        Enumerator.Val(Pat.Var(Name("ys")), Name("free"))
+      ),
+      Name("x")
+    )
+    checkVars(code, Set("free", "xs"))
+  }
+
+  test("test freeVars for yield") {
+    val code = ForYield(
+      List(
+        Enumerator.Generator(Pat.Var(Name("x")), Name("xs")),
+        Enumerator.Val(Pat.Var(Name("ys")), Name("free"))
+      ),
+      Name("x")
+    )
+    checkVars(code, Set("free", "xs"))
+  }
+
+  test("test freeVars while") {
+    val code = While(
+      Name("cond"),
+      Name("body")
+    )
+    checkVars(code, Set("cond", "body"))
+  }
+
+  test("test freeVars do") {
+    val code = Do(
+      Name("body"),
+      Name("cond")
+    )
+    checkVars(code, Set("body", "cond"))
+  }
+
+  test("test freeVars match single case") {
+    val code = Match(
+      Name("free"),
+      List(
+        Case(
+          Pat.Var(Name("x")),
+          Some(Name("cond")),
+          Name("x")
+        )
+      )
+    )
+    checkVars(code, Set("free", "cond"))
+  }
+
+  test("test freeVars match multiple cases") {
+    val code = Match(
+      Name("free"),
+      List(
+        Case(
+          Pat.Typed(Pat.Var(Name("i")), tInt),
+          None,
+          Name("i")
+        ),
+        Case(
+          Pat.Var(Name("x")),
+          Some(Name("cond")),
+          Name("x")
+        )
+      )
+    )
+    checkVars(code, Set("free", "cond"))
+  }
+
+  test("test freeVars partial function") {
+    val code = PartialFunction(
+      List(
+        Case(
+          Pat.Typed(Pat.Var(Name("name")), tInt),
+          None,
+          Name("name")
+        ),
+        Case(
+          Pat.Wildcard(),
+          Some(Name("cond")),
+          Name("x")
+        )
+      )
+    )
+    checkVars(code, Set("cond", "x"))
+  }
+
+  test("test freeVars new") {
+    val code = New(
+      Init(
+        tInt,
+        Name("Int"),
+        List(
+          List(
+            Name("arg1"),
+            Name("arg2")
+          ),
+          List(
+            Name("arg3"),
+            Name("arg4")
+          )
+        )
+      )
+    )
+    checkVars(code, Set("arg1", "arg2", "arg3", "arg4"))
+  }
+
+  test("test freeVars interpolate") {
+    val code = Interpolate(
+      Name("s"),
+      List(
+        Lit.String("hello "),
+        Lit.String("body"),
+        Lit.String("")
+      ),
+      List(
+        Name("x"),
+        Name("y")
+      )
+    )
+    checkVars(code, Set("s", "x", "y"))
+  }
+
+  test("test freeVars annotate") {
+    val code = Annotate(
+      Name("free"),
+      List(
+        Mod.Annot(
+          Init(
+            tInt,
+            Name("hello"),
+            List(
+              List(
+                Name("arg")
+              )
+            )
+          )
+        )
+      )
+    )
+    checkVars(code, Set("free", "arg"))
+  }
+
+  test("test freeVars repeated") {
+    val code = Repeated(
+      Name("x")
+    )
+    checkVars(code, Set("x"))
+  }
+
+  test("test freeVars complex") {
+    val code1 =
+      """
+        |{
+        |  val (a, b) = x
+        |  var Set(str1, str2) = a
+        |  val fun: Any => Any = _ => b
+        |  var bar = "fun"
+        |  {
+        |    var foo = 10
+        |    foo match {
+        |      case 0 => 0
+        |      case num => num + 1
+        |    }
+        |    bar = i
+        |  }
+        |  foo = 42
+        |  this.anno = 12
+        |}
+        |""".stripMargin
+    val tree1 = code1.parse[Term].get
+    checkVars(tree1, Set("x", "i", "foo", "Set"))
+
+    val code2 =
+      """
+        |{
+        |  val fac = num match {
+        |    case 0 => 1
+        |    case _ => num * factorial(num -1)
+        |  }
+        |}
+        |""".stripMargin
+    val tree2 = code2.parse[Term].get
+    checkVars(tree2, Set("num", "factorial"))
+  }
+
+  private def checkVars(code: Tree, expectedFree: Set[Name]): Unit = {
+    val free = EvalHelper.freeVars(code)
     assert(free == expectedFree)
   }
 
