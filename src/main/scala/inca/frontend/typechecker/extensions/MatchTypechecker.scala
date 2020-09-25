05a7e195e50d7221731bc16872b2084a78041c5c
Ronja Schnur <rschnur@students.uni-mainz.de>
2020-09-25 15:20:44 +0200

    :sparkles: Match typechecker

--- src/main/scala/inca/frontend/typechecker/extensions/MatchTypechecker.scala
+++ src/main/scala/inca/frontend/typechecker/extensions/MatchTypechecker.scala
@@ -2,7 +2,13 @@ package inca.frontend.typechecker.extensions
 
 import inca.frontend.core.Core._
 import inca.frontend.extensions._
-import inca.frontend.typechecker.{CoreTypechecker, TypeContext, TypeError, TypeWarning, TypecheckerExtension}
+import inca.frontend.typechecker.{
+  CoreTypechecker,
+  TypeContext,
+  TypeError,
+  TypeWarning,
+  TypecheckerExtension
+}
 import inca.frontend.core.Core
 import inca.frontend.util.TypeHelper
 
@@ -11,95 +17,101 @@ import inca.frontend.util.TypeHelper
   * @author  Ronja Schnur (rschnur@students.uni-mainz.de)
   *          Julian Cichorius (jcichori@students.uni-mainz.de)
   */
-object MatchTypechecker extends TypecheckerExtension 
-{
+object MatchTypechecker extends TypecheckerExtension {
 
-  def typecheck(c : TNode, pb : PatternBinding)(implicit context: TypeContext) : CoreTypechecker.TypeEnvironment = {
-    val PatternBinding(field, pattern) = pb 
-    
+  def typecheck(c: TNode, pb: PatternBinding)(implicit
+      context: TypeContext
+  ): CoreTypechecker.TypeEnvironment = {
+    val PatternBinding(field, pattern) = pb
+
     typechecker.lmi.links.get((c.prettyprint, field)) match {
-      case None => 
-      case Some(value) => 
+      case None =>
+        typechecker.errors.addOne(
+          TypeError(
+            s"TNode ${c.prettyprint} has no field $field (${typechecker.where})."
+          )
+        )
+        TUnit
+      case Some(typ) =>
+        pb.typed(TypeHelper.decode(typ.toString))
     }
-    pb.typed(???)
 
-    typecheck(pb)._2
-
-    ???
+    val (_, env) = typecheck(pattern)
+    env
   }
 
-  private def typecheck(pat : Pattern)(implicit context: TypeContext) : (TypeAnno, CoreTypechecker.TypeEnvironment) = {
+  private def typecheck(pat: Pattern, estm: TypeAnno = TAny)(implicit
+      context: TypeContext
+  ): (TypeAnno, CoreTypechecker.TypeEnvironment) = {
     pat match {
-      case NodePattern(c, bindings)  =>
-        if (!typechecker.subtype(matchee_t, TAnyLinked)) {
-          typechecker.warnings.addOne(TypeWarning(s"NodePattern will never be matched because $matchee_t is not a TLinked"))
-        }
-        val (names, pats) = bindings.map(pb => (pb.field, pb.pattern)).unzip
-        val fieldTypes = names.map { name =>
-          typechecker.lmi.links.get((c.prettyprint, name)) match {
-            case None =>
-              typechecker.errors.addOne(TypeError(s"TNode ${c.prettyprint} has no field $name"))
-              TUnit
-            case Some(typ) =>
-              TypeHelper.decode(typ.toString)
-          }
-        }
-        val env = pats.zip(fieldTypes).foldLeft(context.tenv) {
-          case (e, (p, t)) => typecheck(t, p)(new TypeContext(context, e))._2
-        }
-        (matchee_t, env)
+      case NodePattern(c, bindings) =>
+        val envs = bindings.map(typecheck(c, _))
+        (c, envs.foldLeft(context.tenv)({case (a, b) => typechecker.union(a, b)}))
 
-      case TuplePattern(pats) => matchee_t match {
-        case TTuple(ts) if (pats.length == ts.length) => 
-          val ne = ts.zip(pats).map(x => {
-            val (_, ev) = typecheck(x._1, x._2)
-            ev
-          }).foldLeft[CoreTypechecker.TypeEnvironment](context.tenv)({case (a, b) => typechecker.union(a, b)})
-          (matchee_t, ne)
-        case _ => 
-          typechecker.errors.addOne(TypeError(s"Matchee is not a Tuple or sizes do not match (${typechecker.where})."))
-          (matchee_t, context.tenv)
-      }
-      case VarPattern(name) => 
-        if (context.tenv.contains(name))  {
-          typechecker.errors.addOne(TypeError(s"Variable name is already in use (${typechecker.where}, Code: 0x01)."))
-          (matchee_t, context.tenv)
+      case TuplePattern(pats) =>
+        val (tp, tenvs) = pats.map(typecheck(_)).unzip
+        (
+          TTuple(tp),
+          tenvs.foldLeft(context.tenv)({ case (a, b) => typechecker.union(a, b) })
+        )
+      case VarPattern(name) =>
+        if (context.tenv.contains(name)) {
+          typechecker.errors.addOne(
+            TypeError(
+              s"Variable name is already in use (${typechecker.where}, Code: 0x01)."
+            )
+          )
+          (estm, context.tenv)
+        } else
+          (estm, context.tenv + (name -> estm))
+      case NamedPattern(name, pat) => // ???
+        if (context.tenv.contains(name)) {
+          typechecker.errors.addOne(
+            TypeError(
+              s"Variable name is already in use (${typechecker.where}, Code: 0x02)."
+            )
+          )
+          (estm, context.tenv)
+        } else {
+          val (t, ne) = typecheck(pat, estm)
+          (t, typechecker.union(context.tenv + (name -> t), ne))
         }
-        else 
-          (matchee_t, context.tenv + (name -> matchee_t))
-      case NamedPattern(name, pat) =>
-        if (context.tenv.contains(name))  {
-          typechecker.errors.addOne(TypeError(s"Variable name is already in use (${typechecker.where}, Code: 0x02)."))
-          (matchee_t, context.tenv)
-        }
-        else {
-          val (t, ne) = typecheck(matchee_t, pat)
-          (matchee_t, typechecker.union(context.tenv + (name -> matchee_t), ne))
-        }
-      case LiteralPattern(v) => 
+      case LiteralPattern(v) =>
         val t = typechecker.typecheck(v)
-        if (t != matchee_t)
-          typechecker.errors.addOne(TypeError(s"Unable to match type $matchee_t with $t (${typechecker.where})."))
-        (matchee_t, context.tenv)
-      case WildcardPattern => (matchee_t, context.tenv)
+        if (t != estm)
+          typechecker.errors.addOne(
+            TypeError(s"Unable to match type $estm with $t (${typechecker.where}).")
+          )
+        (t, context.tenv)
+      case WildcardPattern => (estm, context.tenv)
       case _ =>
-        typechecker.errors.addOne(TypeError(s"Error occured ${pat.prettyprint("")} (${typechecker.where})."))
-        (matchee_t, context.tenv)
+        typechecker.errors.addOne(
+          TypeError(s"Error occured ${pat.prettyprint("")} (${typechecker.where}).")
+        )
+        (estm, context.tenv)
     }
   }
 
-  private def typecheck(matchee_t : TypeAnno, cs: Case)(implicit context: TypeContext) : TypeAnno = {
+  private def typecheck(matchee_t: TypeAnno, cs: Case)(implicit
+      context: TypeContext
+  ): TypeAnno = {
     val Case(pattern, body) = cs
-    val (t, te) = typecheck(matchee_t, pattern)
+    val (t, te) = typecheck(pattern, matchee_t)
+
+    if (!typechecker.subtype(t, matchee_t))
+      typechecker.errors.addOne(TypeError(s"Match error (${typechecker.where})."))
+    
     typechecker.typecheck(body)(new TypeContext(context, te))
   }
 
-  override def typecheck(s: Core.Statement, last_in_body: Boolean)(implicit context: TypeContext): (Option[Core.TypeAnno], CoreTypechecker.TypeEnvironment, Boolean) = {
+  override def typecheck(s: Core.Statement, last_in_body: Boolean)(implicit
+      context: TypeContext
+  ): (Option[Core.TypeAnno], CoreTypechecker.TypeEnvironment, Boolean) = {
     s match {
       case Match(matchee, cases) => {
         val (t, te) = typechecker.typecheck(matchee)
-        val return_types  = cases.map(typecheck(t, _))
-        
+        val return_types = cases.map(typecheck(t, _))
+
         if (last_in_body) {
           // check if all return values are the same
           if (return_types.exists(x => !typechecker.subtype(return_types.head, x)))
@@ -108,13 +120,13 @@ object MatchTypechecker extends TypecheckerExtension
                 s"Body has multiple return values (${typechecker.where})."
               )
             )
-  
-          val rt = if (return_types.nonEmpty) return_types.head
-                    else TUnit
-          
+
+          val rt =
+            if (return_types.nonEmpty) return_types.head
+            else TUnit
+
           (Some(rt), te, true)
-        }
-        else 
+        } else
           (None, te, true)
       }
       case _ => (None, context.tenv, false)
