bc7660afe78adbd27b70352210000167eff14b15
Moritz Schmidtgen <mschmi10@students.uni-mainz.de>
2021-02-11 15:07:24 +0100

    remove unused imports

--- src/test/scala/inca/metamodel/MetaModelTest.scala
+++ src/test/scala/inca/metamodel/MetaModelTest.scala
@@ -1,12 +1,6 @@
 package inca.metamodel
-
-import inca.metamodel.MetaModel
-import io.circe.Json
 import org.scalatest.funsuite.AnyFunSuite
-import io.circe._
 import org.scalatest.matchers.should.Matchers._
-import io.circe.parser._
-import io.circe.optics.JsonPath._
 import truechange.{JavaLitType, ListType, OptionType, SortType}
 
 class MetaModelTest extends AnyFunSuite {
