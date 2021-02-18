41a1c460a2cbb60b9e2204a4956525b3ebf22b10
Sebastian Erdweg <seba.github@gmail.com>
2021-02-18 10:02:39 +0100

    improve souffle benchmark

--- souffle-importer/src/test/scala/inca/souffle/TestSouffleVarPointsTo.scala
+++ souffle-importer/src/test/scala/inca/souffle/TestSouffleVarPointsTo.scala
@@ -3,13 +3,31 @@ package inca.souffle
 import inca.runtime.EnginePool
 import inca.runtime.Query.Matcher
 import inca.runtime.context.QueryScope
-import org.eclipse.viatra.query.runtime.rete.matcher.DRedReteBackendFactory
+import org.eclipse.viatra.query.runtime.rete.matcher.{DRedReteBackendFactory, TimelyReteBackendFactory}
 import org.scalatest.flatspec.AnyFlatSpec
 import truechange.EditScript
 
 import scala.io.Source
 
 class TestSouffleVarPointsTo extends AnyFlatSpec {
+  val expectedTupleCount = Map(
+    "selfcontained_basic_SubtypeOf" -> 13487,
+    "selfcontained_basic_SupertypeOf" -> 13487,
+    "selfcontained_VarPointsTo0" -> 34291,
+    "selfcontained_VarPointsTo" -> 511391,
+    "selfcontained_VarPointsTo0" -> 34291,
+    "selfcontained_Assign" -> 31754,
+    "selfcontained_Assign0" -> 19494,
+    "selfcontained_InterProc0" -> 190648,
+    "selfcontained_InterProc1" -> 1719,
+    "selfcontained_InterProc2" -> 11101,
+    "selfcontained_StaticFieldPointsTo" -> 705,
+    "selfcontained_InstanceFieldPointsTo" -> 354511,
+    "selfcontained_Reachable" -> 2875,
+    "selfcontained_CallGraphEdge" -> 13136,
+    "selfcontained_ArrayIndexPointsTo" -> 5636
+  )
+
   "var points to souffle analysis" should "derive correct number of tuples" in {
     println(System.getProperty("user.dir"))
     val benchmarkPath = "souffle-importer/benchmark"
@@ -22,16 +40,6 @@ class TestSouffleVarPointsTo extends AnyFlatSpec {
     val compiledModule = compiler.compile("selfcontained", analysis)
 
     val psModule = compiledModule.psystemModule
-    val queryScope = new QueryScope(compiledModule.options.languageMetaInfo, Seq())
-
-    def getMatcher(fun: String): Matcher = {
-      val querySpec = psModule.patterns.getOrElse(fun, throw new IllegalArgumentException(s"Function $fun undefined in module."))
-      EnginePool.loadQuery(querySpec(), queryScope, DRedReteBackendFactory.INSTANCE)
-    }
-
-    // Use DRed because initialization is faster
-    val (engine, database) = EnginePool.loadEngineAndDatabase(queryScope, DRedReteBackendFactory.INSTANCE)
-
     val startLoadFactFiles = System.currentTimeMillis()
     val edits = compiledModule.inputs.flatMap { case (sig, input) =>
       val inputCompiler = new SouffleInputToEditscript(s"$benchmarkPath/minijavac")
@@ -44,45 +52,43 @@ class TestSouffleVarPointsTo extends AnyFlatSpec {
     val editScript = EditScript(edits)
     println(editScript.size)
 
-    val matchers = compiledModule.printSizes.map(ps => getMatcher(ps.name))
 
-    val startQuery = System.currentTimeMillis()
-    var loadingTime: Long = 0
-    engine.delayUpdatePropagation {() =>
-      val startLoadDB = System.currentTimeMillis()
-      database.processEditScript(editScript)
-      val endLoadDB = System.currentTimeMillis()
-      loadingTime = endLoadDB - startLoadDB
-    }
-    val endQuery = System.currentTimeMillis()
-    println(s"Time to fill database: ${loadingTime}ms")
-    println("Used memory: " + MeasurementUtils.usedMemoryInMBytes())
-    println(s"Time to process query: ${endQuery - startQuery}ms")
+    val RUNS = 5
 
-    matchers.foreach { m =>
-      println(s"${m.getPatternName}: ${m.countMatches()}")
-    }
+    for (run <- 1 to RUNS) {
+      val queryScope = new QueryScope(compiledModule.options.languageMetaInfo, Seq())
+      val (engine, database) = EnginePool.loadEngineAndDatabase(queryScope, DRedReteBackendFactory.INSTANCE)
 
-    val expectedTupleCount = Map(
-      "selfcontained_basic_SubtypeOf" -> 13487,
-      "selfcontained_basic_SupertypeOf" -> 13487,
-      "selfcontained_VarPointsTo0" -> 34291,
-      "selfcontained_VarPointsTo" -> 511391,
-      "selfcontained_VarPointsTo0" -> 34291,
-      "selfcontained_Assign" -> 31754,
-      "selfcontained_Assign0" -> 19494,
-      "selfcontained_InterProc0" -> 190648,
-      "selfcontained_InterProc1" -> 1719,
-      "selfcontained_InterProc2" -> 11101,
-      "selfcontained_StaticFieldPointsTo" -> 705,
-      "selfcontained_InstanceFieldPointsTo" -> 354511,
-      "selfcontained_Reachable" -> 2875,
-      "selfcontained_CallGraphEdge" -> 13136,
-      "selfcontained_ArrayIndexPointsTo" -> 5636
-    )
+      def getMatcher(fun: String): Matcher = {
+        val querySpec = psModule.patterns.getOrElse(fun, throw new IllegalArgumentException(s"Function $fun undefined in module."))
+        EnginePool.loadQuery(querySpec(), queryScope, TimelyReteBackendFactory.FIRST_ONLY_SEQUENTIAL)
+      }
 
-    matchers.foreach { m =>
-      assert(m.countMatches() == expectedTupleCount(m.getPatternName))
+      val matchers = compiledModule.printSizes.map(ps => getMatcher(ps.name))
+
+      val startQuery = System.currentTimeMillis()
+      var loadingTime: Long = 0
+      engine.delayUpdatePropagation { () =>
+        val startLoadDB = System.currentTimeMillis()
+        database.processEditScript(editScript)
+        val endLoadDB = System.currentTimeMillis()
+        loadingTime = endLoadDB - startLoadDB
+      }
+      val endQuery = System.currentTimeMillis()
+
+      matchers.foreach { m =>
+        assert(m.countMatches() == expectedTupleCount(m.getPatternName))
+      }
+
+      println(s"Run $run: Time to fill database: ${loadingTime}ms")
+      println(s"Run $run: Used memory: ${MeasurementUtils.usedMemoryInMBytes()}MB")
+      println(s"Run $run: Time to process query: ${endQuery - startQuery}ms")
+
+      //    matchers.foreach { m =>
+      //      println(s"${m.getPatternName}: ${m.countMatches()}")
+      //    }
+      System.gc()
+      Thread.sleep(500)
     }
 
 // INCREMENTAL UPDATES
