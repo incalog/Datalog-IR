3073b97d7d05f8b5acdb90e986c959aab4d44853
Moritz Schmidtgen <mschmi10@students.uni-mainz.de>
2021-02-18 16:41:55 +0100

    Create a new frontend using the imported metamodel if created before. Else use the fronted given by options, to stay consistent with previous scenarios.

--- src/main/scala/inca/compiler/CompiledFunModule.scala
+++ src/main/scala/inca/compiler/CompiledFunModule.scala
@@ -2,14 +2,22 @@ package inca.compiler
 
 import inca.backend.ir.GP
 import inca.backend.ir.GP.Name
-import inca.frontend.core.CompileToGP
+import inca.frontend.core.{CompileToGP, Frontend}
 import inca.frontend.core.tree._
 import inca.frontend.desugar.Desugar
 import inca.frontend.parser.SourceLocation
+import inca.metamodel.MetaModel
 
 case class CompiledFunModule(fun: Module, options: Options) extends CompiledModule {
 
   override def name: Name = fun.name.name
+
+  def metaModelName = fun.usingMetaModel.name.name
+
+  val frontend: Frontend = if(metaModelName.isEmpty) options.frontend else {
+    val metaModel = new MetaModel("./src/test/scala/inca/analyzedLangs/GoLang.json", "./src/test/scala/inca/analyzedLangs/tokenNodes" + metaModelName)
+    options.frontendFactory(metaModel.getLanguageMetaInfo)
+  }
 
   override def sourceLocation: SourceLocation = fun.name
 
