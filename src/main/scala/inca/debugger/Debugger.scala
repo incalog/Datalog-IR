00e4306037693bebedf8c3a5ade6cab047224a2c
Roshnal Lihinikadu <roshnal15@gmail.com>
2021-03-07 22:19:54 +0100

    Fixes, refactoring, initial stepper for core lang, env, rudimentary console

--- src/main/scala/inca/debugger/Debugger.scala
+++ src/main/scala/inca/debugger/Debugger.scala
@@ -1,116 +1,165 @@
 package inca.debugger
 
 import inca.compiler.CompiledFunModule
-import inca.frontend.core.tree.{Param, PatternFunction}
-import inca.runtime.Query.Matcher
+import inca.frontend.core.tree._
 import inca.runtime.{Database, DatabaseAccessor}
-import truechange.{Type, URI}
+import inca.runtime.Query.Matcher
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
-import scala.jdk.CollectionConverters.ListHasAsScala
 
-// FIXME choose more meaningful names, reconsider access modifiers/qualifiers
 class Debugger(feed: Database, matcher: Matcher, module: CompiledFunModule) {
-  private val db = new DatabaseAccessor(feed)
+  private[debugger] val db = new DatabaseAccessor(feed)
+  private[debugger] val debugEnv = new Environment(matcher.getPatternName.replace(module.fun.name + "_", ""), module)
 
-  private[debugger] val funParams: Map[String, Seq[Param]] = module.fun.content.map({
-    case pf: PatternFunction => (pf.name.name, pf.params)
-  }).toMap
+  private var cf = debugEnv.funName // "Current" function
 
-  private[debugger] val env: mutable.Map[String, Set[ColumnValue]] = mutable.Map()
+  private val pntr: mutable.Map[Name, Int] = mutable.Map(cf -> 0) // Pointers for each function traversed
+  private val callers: mutable.Map[Name, Name] = mutable.Map() // Caller for each function
 
-  val matches: Seq[Match] = {
-    val funName = matcher.getPatternName.replace(module.fun.name + "_", "")
+  val matches = new Matches(db, matcher, debugEnv)
 
-    val bufMatches = new ListBuffer[Match]
-    matcher.forEachMatch(mat => {
-      val cols = mat.parameterNames().asScala.map(colName => {
-        val colValue = mat.get(colName) match {
-          case uri: URI => URIValue(uri, getNodeTypes(uri))
-          case scalaType => ScalaValue(scalaType)
-        }
-        val colType = getColType(funName, colName)
-        Column(colName, colValue, colType)
+  def allMatches: Seq[Match] = matches.matches
 
-      }).toSeq
-      bufMatches += Match(mat, cols)
-    })
-
-    bufMatches.toList
-  }
-
-  def load(mat: Match): Unit = { // Not finalized yet! Name should also change
+  def load(mat: Match): Unit = {
+    debugEnv.env += debugEnv.funName -> mutable.Map()
     mat.inputs.foreach(col => {
-      env.updateWith(col.name) {
+      debugEnv.env(debugEnv.funName).updateWith(Name(col.name)) {
         case Some(v) => Some(v + col.value)
         case None => Some(Set(col.value))
       }
     })
-
-    println(env)
   }
 
-  private def getColType(funName: String, colName: String): ColumnType = {
-    val params = funParams.getOrElse(funName,
-      throw new IllegalArgumentException(s"Function $funName not defined in module"))
+  private[debugger] def stepOver(): Unit = {
+    val p = pntr(cf)
+    if(p > 0) parseStmt(cf, debugEnv.stmts(cf)(p - 1))
 
-    if(params.map(p => p.name.name).contains(colName)) // Function funName defines paramName as parameter
-      Input
-    else
-      Output
+    if(p < debugEnv.stmts(cf).length) {
+      println(":> " + debugEnv.typedStmts(cf)(p))
+      pntr(cf) += 1
+    } else
+      stepOut()
   }
 
-  private def getNodeTypes(uri: URI): Seq[Type] = db.nodeInstancesByValue(uri).keys.toSeq
+  private[debugger] def stepInto(): Unit = {
+    val callExp = debugEnv.stmts(cf)(pntr(cf) - 1).ensureCore match {
+      case Assign(_, exp) => exp
+      case Assert(cond) => cond
+      case Yield(exp) => exp
+      case _ => throw new IllegalArgumentException("Statement has no function calls")
+    }
 
+    val fun = callExp.ensureCore match {
+      case Call(name, args, _) =>
+        initFunParams(name, args.map(arg => parseExp(cf, arg)))
+        name
+      case _ => throw new IllegalArgumentException("Statement has no function calls")
+    }
 
-  def printMatches(tagDepth: Int = 0): String = {
-    val builder = new StringBuilder
-    builder.append("Matches: " + matches.size)
+    pntr += fun -> 0
+    callers += fun -> cf
 
-    matches.foreach(mat => builder.append(printMatch(mat, tagDepth)))
-    builder.toString
+    cf = fun
+
+    println("Stepping into: " + fun + " Pointers: " + pntr + " Callers: " + callers)
+    stepOver()
   }
 
-  def printMatch(mat: Match, tagDepth: Int = 0): String = {
-    mat.cols.map {
-      col: Column => "\"" + col.name + "[" + col.ty + "]" +  "\"=" + (col.value match {
-        case uriVal: URIValue => uriVal.uri + {
-          if(tagDepth <= 0) ""
-          else ":" + getTag(uriVal.uri, tagDepth)
-        }
-        case scalaVal: ScalaValue => scalaVal.prettyPrint()
+  private[debugger] def stepOut(): Unit = {
+    println("Stepping out of : " + cf + " Pointers: " + pntr + " Callers: " + callers)
+
+    pntr(cf) = 0
+    cf = callers.getOrElse(cf, {
+      println("End of program reached")
+      cf
+    })
+
+    stepOver() // FIXME Ugly hack? Idk. Terminator map?
+  }
+
+
+  private def parseStmt(fun: Name, s: Statement): Set[ColumnValue] = s.ensureCore match {
+    case Assign(names, exp) =>
+      debugEnv.env(fun) += names.head -> parseExp(fun, exp) // TODO Multiple assignments
+      Set()
+
+    case Yield(exp) => parseExp(fun, exp)
+
+    case Assert(cond) => Set()
+    case Values(name, typ) => Set()
+    case FailStatement => Set()
+  }
+
+  private def parseExp(fun: Name, e: Expression): Set[ColumnValue] = e.ensureCore match {
+    case Var(name) => debugEnv.env(fun)(name)
+    case Constant(lit) => Set(ScalaValue(lit))
+
+    case PathAccess(receiver, link) =>
+      parseExp(fun, receiver).flatMap(cv => cv match {
+        case uriValue: URIValue => getLinks(uriValue, link)
+        case _ => Set()
       })
-    }.mkString("\nMatch { ", ", ", " }")
+
+    case Call(name, args, transitive) =>
+      initFunParams(name, args.map(arg => parseExp(fun, arg)))
+      run(name)
+
+    case Cast(src, targetTyp) =>
+      if(parseExp(fun, src).forall(cv => isOfType(cv, targetTyp)))
+        parseExp(fun, src) // Check and ensure cast-ability?
+      else Set()
+
+    case InstanceOf(exp, ty) => Set(ScalaValue(parseExp(fun, exp).forall(cv => isOfType(cv, ty)))) // Confirm behavior
+    case NotInstanceOf(exp, ty) => Set(ScalaValue(parseExp(fun, exp).forall(cv => !isOfType(cv, ty))))
+
+    case Eq(lhs, rhs) => Set(ScalaValue(parseExp(fun, lhs) == parseExp(fun, rhs))) // Confirm primitives/ScalaVals
+    case Neq(lhs, rhs) => Set(ScalaValue(parseExp(fun, lhs) != parseExp(fun, rhs)))
+
+    case Def(exp) => parseExp(fun, exp) // ??
+
+    case Count(call) => Set(ScalaValue(parseExp(fun, call).size)) // ??
+    case Tuple(exps) => Set(ScalaValue(exps.flatMap(e => parseExp(fun, e)))) // ??
+
+    case _ => Set()
   }
 
-  private def getTag(uri: URI, d: Int): String = {
-    if(d > 0) {
-      val ts = db.linkNodeInstancesByValue1(uri).keys.map(link => stripTag(link._1)) // Link (Tag:String, name:String)
-
-      if(ts.isEmpty) { // Primitive type
-        val pts = db.linkPrimitiveInstancesByValue1(uri)
-        pts.keys.map(link => stripTag(link._1) + (
-          if(d == 1) ""
-          else pts.get(link) match { // Get primitive value(s)
-              case None => "()"
-              case Some(m) => m.index(uri.asInstanceOf[URI]).mkString("(", ", ", ")")
-            })
-        ).mkString("|")
-
-      } else { // Node-type. Recurse over the linked nodes to get their tags
-        val childNodes = db.linkNodeInstancesByValue1(uri).values
-          .map(idx => getTag(idx.index.get(uri), d - 1))
-          .filter(_.trim.nonEmpty)
-
-        ts.mkString("|") + (if(childNodes.nonEmpty) childNodes.mkString("(", ", ", ")") else "")
-      }
-
-    } else ""
+  private def initFunParams(fun: Name, args: Seq[Set[ColumnValue]]): Unit = {
+    debugEnv.env += fun -> mutable.Map()
+    debugEnv.funParams(fun).zip(args).foreach {
+      case (param, arg) => debugEnv.env(fun) += param.name -> arg
+    }
   }
 
-  private def stripTag(tag: String): String = tag.substring(tag.findLast(_.equals('.')) match {
-    case Some(_) => tag.lastIndexOf('.') + 1
-    case None => 0
-  })
+  private def run(fun: Name): Set[ColumnValue] = { // TODO Multiple outputs
+    debugEnv.stmts(fun).flatMap(stmt => parseStmt(fun, stmt)).toSet
+  }
+
+  private def getLinks(uriValue: URIValue, link: Link): Set[ColumnValue] = {
+    val lnkNodes = db.linkNodeInstances.filter {
+      case (lnk, uris) => lnk._2 == link.prettyprint && uris.index.containsKey(uriValue.uri)
+    }
+
+    if(lnkNodes.isEmpty) {
+      db.linkPrimitiveInstancesByValue1(uriValue.uri).filter {
+        case (lnk, _) => lnk._2 == link.prettyprint
+      }.flatMap {
+        case (_, vals) => vals.index(uriValue.uri).toSeq.map(prim => ScalaValue(prim))
+      }.toSet
+
+    } else {
+      lnkNodes.map {
+        case (_, uris) =>
+          val uri = uris.index.get(uriValue.uri)
+          URIValue(uri, db.nodeInstancesByValue(uri).keys.toSeq)
+      }.toSet
+    }
+  }
+
+  private def isOfType(cv: ColumnValue, ty: Type): Boolean = {
+    /*println("COMP Type " + (cv match {
+      case u: URIValue => u.types.mkString(",")
+      case sv: ScalaValue => sv.v
+    }) + " TO " + ty)*/
+    true // TODO
+  }
 }
