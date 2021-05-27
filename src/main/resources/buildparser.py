55f16e19de9ca53ba147ea688f7f8bf9abc95fe1
Moritz Schmidtgen <mschmi10@students.uni-mainz.de>
2021-05-27 11:21:39 +0200

    Add script to create binaries from tree-sitter grammars.

---
+++ src/main/resources/buildparser.py
@@ -0,0 +1,5 @@
+from tree_sitter import Language, Parser 
+
+#Language.build_library('/home/moritz/Desktop/tree-sitter-python.so', ['/home/moritz/Desktop/tree-sitter-python'])
+#Language.build_library('/home/moritz/Desktop/tree-sitter-go.so', ['/home/moritz/Desktop/tree-sitter-go'])
+Language.build_library('/home/moritz/Desktop/tree-sitter-java.so', ['/home/moritz/Desktop/tree-sitter-java'])
