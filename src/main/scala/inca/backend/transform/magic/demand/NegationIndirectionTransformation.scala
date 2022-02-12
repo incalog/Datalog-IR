7864118fce66ce33ac2a00080047540be7534944
Laura Stingl <lstingl@students.uni-mainz.de>
2022-02-12 11:31:32 +0100

    add negative indirection for demand transformation

---
+++ src/main/scala/inca/backend/transform/magic/demand/NegationIndirectionTransformation.scala
@@ -0,0 +1,37 @@
+package inca.backend.transform.magic.demand
+
+import inca.backend.ir.Datalog
+import inca.backend.ir.Datalog.{Body, Call, Module, Pattern}
+import inca.backend.transform.{Transformation, Transformer}
+import inca.runtime.context.DataModel
+import inca.backend.hints.MagicSetHints.NegativeIndirectionRelation
+
+
+object NegationIndirectionTransformation extends Transformation {
+
+  def negativeIndirectionName(name: String): String = "negative$" + name
+
+  override def transformer(dataModel: DataModel): Transformer = new Transformer {
+    var negativelyCalledPatterns: Set[Call] = Set()
+
+    override def transformModule(module: Module): Module = {
+      val transformedPats = module.pats.flatMap(transformPattern)
+      val patterns = module.pats.map(p => p.name -> p).toMap
+      val negIndirectPatterns = negativelyCalledPatterns.map { call =>
+        val pat = patterns(call.name)
+        Pattern(pat.vis, negativeIndirectionName(call.name), pat.params, Seq(Body(Seq(
+          call.copy().withHints(call) // negative call of $name
+        )))).addHint(NegativeIndirectionRelation)
+      }
+      val newpats = transformedPats ++ negIndirectPatterns
+      Module(module.name, module.imports, newpats, module.scalaContent)
+    }
+
+    override def transformAtom(atom: Datalog.Atom): Seq[Datalog.Atom] = (atom match {
+      case x@Call(name, args, transitive, true) =>
+        negativelyCalledPatterns += x
+        Seq(Call(negativeIndirectionName(name), args.map(transformTerm), transitive, neg = false))
+      case _ => super.transformAtom(atom)
+    }).map(_.withHints(atom))
+  }
+}
