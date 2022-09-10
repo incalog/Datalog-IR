037dea441dd39cbbfb076268c9d92fd403193053
lleilich <lleilich@students.uni-mainz.de>
2022-09-10 16:58:16 +0200

    registered annotator

--- Inca Plugin/src/main/java/language/FuncIncaUtil.java
+++ Inca Plugin/src/main/java/language/FuncIncaUtil.java
@@ -20,8 +20,9 @@ import java.util.*;
  * General util class. Provides methods for finding named nodes in the Psi tree.
  */
 public class FuncIncaUtil {
-
-    public static List<PsiNamedElement> findFunDefs(@NotNull Project project, @Nullable FuncIncaFile f, @Nullable String name){
+    /*
+    * finds all Psi nodes named "name"*/
+    public static List<PsiNamedElement> findDefinitionNode(@NotNull Project project, @Nullable FuncIncaFile f, @Nullable String name){
         List<PsiNamedElement> res = new ArrayList<>();
         Collection<VirtualFile> virtualFiles = FileTypeIndex.getFiles(FuncIncaFileType.INSTANCE, GlobalSearchScope.projectScope(project));
         return res;
