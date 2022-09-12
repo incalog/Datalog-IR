b136193cf7c5cf6f3488d74ed518d4f559376600
lleilich <lleilich@students.uni-mainz.de>
2022-09-12 18:49:41 +0200

    Util functions

--- Inca Plugin/src/main/java/language/FuncIncaUtil.java
+++ Inca Plugin/src/main/java/language/FuncIncaUtil.java
@@ -1,16 +1,12 @@
 package language;
 
-import com.google.common.collect.Lists;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.search.FileTypeIndex;
 import language.psi.*;
-import com.intellij.lang.ASTNode;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.ArrayUtil;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,10 +17,42 @@ import java.util.*;
  */
 public class FuncIncaUtil {
     /*
-    * finds all Psi nodes named "name"*/
-    public static List<PsiNamedElement> findDefinitionNode(@NotNull Project project, @Nullable FuncIncaFile f, @Nullable String name){
+    * finds all Psi Definition nodes named "name" in the whole project. For name = null find all definitions.*/
+    public static List<PsiNamedElement> findDefinitionNode(@NotNull Project project, @Nullable String name, @NotNull PsiNamedElement e){
         List<PsiNamedElement> res = new ArrayList<>();
+        final PsiFile psiFile = e.getContainingFile().getOriginalFile();
+        // this would be the place for getting the imported modules
+
         Collection<VirtualFile> virtualFiles = FileTypeIndex.getFiles(FuncIncaFileType.INSTANCE, GlobalSearchScope.projectScope(project));
+        for (VirtualFile virtualFile : virtualFiles){
+            FuncIncaFile f = (FuncIncaFile) PsiManager.getInstance(project).findFile(virtualFile);
+            final boolean returnAllReferences = (name == null);
+            final boolean inLocalModule = (f != null && f.equals(psiFile));
+            res.addAll(findDefinitionNode(f, name, e));
+        }
+        return res;
+    }
+
+    /*
+    * finds all Psi Definition nodes named "name" in one file*/
+    public static List<PsiNamedElement> findDefinitionNode(@Nullable FuncIncaFile file, @Nullable String name, @Nullable PsiNamedElement e){
+        List<PsiNamedElement> res = new ArrayList<>();
+        if(file == null)
+            return res;
+        // We only want to look for classes that match the element we are resolving
+        final Class<? extends PsiNamedElement> elementClass;
+        if(e instanceof FuncIncaTypeName || e instanceof FuncIncaDataDef)
+            elementClass = FuncIncaDataDef.class;
+        else if(e instanceof FuncIncaConstructorPattern || e instanceof FuncIncaDataConstructor)
+            elementClass = FuncIncaDataConstructor.class;
+        else
+            elementClass = PsiNamedElement.class;
+
+        Collection<PsiNamedElement> namedElements = PsiTreeUtil.findChildrenOfType(file, elementClass);
+        for(PsiNamedElement namedElement: namedElements){
+            if(name == null || name.equals(namedElement.getName()))
+                res.add(namedElement);
+        }
         return res;
     }
 
