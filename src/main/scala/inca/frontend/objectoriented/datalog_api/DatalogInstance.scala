8e5f727422e02f82b62d327ca32857fca0973127
André Pacak <pacak@uni-mainz.de>
2022-11-07 15:12:40 +0100

    WIP generic solver interface

--- src/main/scala/inca/frontend/objectoriented/datalog_api/DatalogInstance.scala
+++ src/main/scala/inca/frontend/objectoriented/datalog_api/DatalogInstance.scala
@@ -1,6 +1,7 @@
 package inca.frontend.objectoriented.datalog_api
 
 import inca.compiler.{CompiledDatalogModule, CompiledModule}
+import inca.frontend.objectoriented.core.TypeCastExpr
 import inca.frontend.objectoriented.datalog_api.datalog_api.RelationName
 import inca.runtime.Query
 import inca.runtime.Query.Specification
@@ -63,7 +64,6 @@ case class Relation2[A1 <: AnyRef, A2 <: AnyRef](name: RelationName, matches: It
   }
   def flattenEntry(entry: Tuple): Seq[AnyRef] = Seq(entry._1, entry._2)
 }
-
 case class Relation3[A1 <: AnyRef, A2 <: AnyRef, A3 <: AnyRef](name: RelationName, matches: Iterable[Query.Match])
   extends Relation
   {
@@ -81,17 +81,17 @@ trait Operation
 case class Insertion(name: String, tuple: Tuple) extends Operation
 case class Deletion(name: String, tuple: Tuple) extends Operation
 
-trait Input {
-  def edits: Option[EditScript]
-  def inputs: Seq[Operation]
-}
+// trait Input {
+//  def edits: Option[EditScript]
+//  def inputs: Seq[Operation]
+// }
 // ...
 // feed.processEditScript(edits)
 // inputs.foreach { o => feed.insert(o.name, o.tuple) }
 
-case class PatternInput(pat: String, tuple: Tuple, edits: Option[EditScript]) extends Input {
-  override def inputs: Seq[Operation] = Seq(Insertion(demandPatternExtensionalPrefix + pat, tuple))
-}
+// case class PatternInput(pat: String, tuple: Tuple, edits: Option[EditScript]) extends Input {
+//   override def inputs: Seq[Operation] = Seq(Insertion(demandPatternExtensionalPrefix + pat, tuple))
+// }
 
 case class EDBInput(insertions: Seq[Relation], deletions: Seq[Relation], edits: Option[EditScript] = None) extends Input {
   override def inputs: Seq[Operation] =
@@ -114,16 +114,17 @@ case class EDBInput(insertions: Seq[Relation], deletions: Seq[Relation], edits: 
 
 // TODO: Edit scripts
 trait EDBChange {
+  def es: EditScript
   def insertions: Map[RelationName, Relation]
   def deletions: Map[RelationName, Relation]
 }
 
-trait Datalog {
+trait DatalogInstance {
   protected def engine: AdvancedViatraQueryEngine
   protected def feed: Database
   protected def compiled: CompiledModule
 
-  def changeEDB(edb: EDBChange): Unit = engine.delayUpdatePropagation { () =>
+  protected def update(edb: EDBChange): Unit = engine.delayUpdatePropagation { () =>
     edb.insertions.foreach { case (name, relation) =>
       relation.entries.foreach { tuple =>
         feed.insert(name, Tuples.flatTupleOf(relation.flattenEntry(tuple)))
@@ -135,8 +136,7 @@ trait Datalog {
       }
     }
   }
-
-  def query(relName: RelationName)(input: Relation = UnitRelation(relName)): Relation = {
+  protected def query(relName: RelationName)(input: Relation = UnitRelation(relName)): Relation = {
     val specification = compiled.psystemModule.patterns(relName)()
     val matcher = specification.getMatcher(engine)
     // TODO: Parameter names
@@ -156,3 +156,106 @@ trait Datalog {
   def toQueryMatch(values: Seq[AnyRef], spec: Specification): Query.Match =
     Query.Match(spec, values.toArray, isMutable = false)
 }
+
+object Test {
+  // Functional
+  val inst = FunctionalInstance(mod)
+  inst.execute("main", Seq(q"1", q"2", q"""Add(Var("x"), 12)"""))
+  // Object Oriented
+  val inst = OOInstance(mod)
+  inst.execute("main", Seq(q"1", q"2", q"""new X(12, 3)"""))
+  // Constaint
+  val inst = ConstraintInstance(mod)
+  //
+  // @diffable trait Exp extends Diffable
+  // case class Add(lhs: Exp, rhs: Exp)
+  val tree = Add(Var("x"), 12)
+  inst.execute("main", tree, edbInserts/Deletions)
+  // IR
+  val inst = IRInstance(mod)
+  inst.execute("main", edbInserts/Deletions)
+  // Souffle
+  val inst = SouffleInstance(mod)
+  inst.execute("main", edbInserts/Deletions OR Editscript)
+
+  val inst = FunctionalInstance(mod)
+  inst.
+}
+
+trait DatalogInstance[I <: Input, R <: Runner] {
+  def engine: AdvancedViatraQueryEngine
+  def database: Database
+
+  protected def updateInput(input: I): Unit = {
+    val edbChange = input.translate
+    engine.delayUpdatePropagation { () =>
+      database.processEditScript(edbChange.es)
+      edbChange.insertions.foreach { case (name, rel) =>
+        rel.entries.foreach {
+          database.insert(name, rel.flattenEntry(_))
+        }
+      }
+      edbChange.deletions.foreach { case (name, rel) =>
+        rel.entries.foreach {
+          database.delete(name, rel.flattenEntry(_))
+        }
+      }
+    }
+  }
+  protected def query(name: String, rel: Relation): Runner = {
+    // get spec
+    // get matcher
+    // get result
+  }
+}
+trait Runner[I <: Input] {
+  def updateInput(input: I): Unit
+}
+class FunctionalRunner(instance: FunctionalInstance[FunctionalInput, FunctionalRunner]) extends Runner {
+  def run(name: String, args: Seq[meta.Scala.Term]): Relation
+}
+class ObjectOrientedRunner(instance: ObjectOrientedInstance[ObjectOrientedInput, ObjectOrientedRunner]) extends Runner {
+  def run(name: String, args: Seq[meta.Scala.Term]): Relation = {
+    if (typeCastError) throw TypeCastEx
+    else rel
+  }
+}
+class IRRunner(instance: DatalogInstance[IRInput, IRRunner]) extends Runner {
+  def run(name: String, rel: Relation): Relation
+}
+//trait EDBChange {
+//  def es: EditScript
+//  def insertions: Seq[Insertion]
+//  def deletions: Seq[Insertion]
+//}
+trait Input {
+  def translate: EDBChange
+}
+case class FunctionalInput() extends Input
+case class ObjectOrientedInput() extends Input
+class FuntionalInstance(...) extends DatalogInstance[FunctionalInput] {
+
+}
+
+// x, y, z
+// 1, 2, 3
+
+// 1, 2, 3
+// 2, 3, 5
+// 7, 3, 9
+inst.changeEDB()
+inst.changeEDB()
+inst.query()
+
+// we need compiled module for these steps
+//   engine
+//   database
+//   queryscope
+
+// oo
+// engine
+// db
+// queryscope
+// fill db with inheritance hierarchy
+// create runner r
+// r.run(main, ....)
