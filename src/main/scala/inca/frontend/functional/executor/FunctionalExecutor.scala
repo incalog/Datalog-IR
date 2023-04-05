434ce7c400e8cafffd685f0963b692e3faf7756e
André Pacak <pacak@uni-mainz.de>
2023-04-05 14:04:20 +0200

    fix functional executors

--- src/main/scala/inca/frontend/functional/executor/FunctionalExecutor.scala
+++ src/main/scala/inca/frontend/functional/executor/FunctionalExecutor.scala
@@ -263,11 +263,21 @@ object FunctionalExecutor {
     def executeInput(main: String, input: Input, deleteInput: Boolean = false): Results[AnyRef] = {
       val (es, tuple) = input
       feed.processEditScript(es)
-      feed.insert(demandPatternExtensionalPrefix + main, tuple)
+      feed.insert(extensionalRelationName(main, tuple), tuple)
       val results = output(main, tuple)
       if (deleteInput)
-        feed.delete(demandPatternExtensionalPrefix + main, tuple)
+        feed.delete(extensionalRelationName(main, tuple), tuple)
       results
+    }
+
+    def extensionalRelationName(main: String, tuple: Tuple): String = {
+      val numBoundParams = tuple.getSize
+      val boundAdorn = (0 until numBoundParams).map(_ => "b")
+      val mainPattern = compiled.optimized.pats.find(_.name == main).getOrElse(throw new IllegalArgumentException(s"WHY $main not exist?"))
+      val numFreeParams =  mainPattern.params.size - numBoundParams
+      val freeAdorn = (0 until numFreeParams).map(_ => "f")
+      val name = demandPatternExtensionalPrefix + main + "$" + (boundAdorn ++ freeAdorn).mkString
+      name
     }
 
     def measure(main: String, args: Seq[meta.Term]): (Long, Long, Long) = {
@@ -285,7 +295,7 @@ object FunctionalExecutor {
       val loadingTime = endLoadDB - startLoadDB
 
       val startInsertQuery = System.nanoTime()
-      feed.insert(demandPatternExtensionalPrefix + main, tuple)
+      feed.insert(extensionalRelationName(main, tuple), tuple)
       val endInsertQuery = System.nanoTime()
 
       println(s"Tuples in $main: ${mainMatcher.getAllMatches().size()}")
