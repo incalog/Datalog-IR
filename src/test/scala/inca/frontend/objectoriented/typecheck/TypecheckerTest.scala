6ee0fd367a77a8ccdd2c263f48d04d83b8142a8f
mgoehrin <mgoehrin@students.uni-mainz.de>
2023-09-01 14:00:33 +0200

    Refactor Test structure

--- src/test/scala/inca/frontend/objectoriented/typecheck/TypecheckerTest.scala
+++ src/test/scala/inca/frontend/objectoriented/typecheck/TypecheckerTest.scala
@@ -72,4 +72,142 @@ class TypecheckerTest extends AnyFunSuite {
     val code = FileUtil.readFile("objectoriented/graphs/BinaryTree.oinca")
     checkModule(code)
   }
+
+  /*---------------*/
+  /* Test Generics */
+  /*---------------*/
+
+  test("Simple Generic Class") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericClass.oinca")
+    checkModule(code)
+  }
+
+  test("Addition of Generic Param Failure") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericClassGenericParamAdditionFailure.oinca")
+    checkModuleErrors(code)
+  }
+
+  test("Generic Class Instances with different types") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericClassInstancesWithDifferentTypes.oinca")
+    checkModule(code)
+  }
+
+  test("Generic Class inner Shadowing") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericClassMethodInnerShadowing.oinca")
+    checkModule(code)
+  }
+
+  test("Generic Class with wrong type annotation") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericClassTypeAnnotationFailure.oinca")
+    checkModuleErrors(code)
+  }
+
+  test("Generic Class with multiple generic Types") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericClassWithMultipleTypes.oinca")
+    checkModule(code)
+  }
+
+  test("Simple generic method") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericMethod.oinca")
+    checkModule(code)
+  }
+
+  test("Generic method with wrong Out-Type annotation") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericMethodOutTypeFailure.oinca")
+    checkModuleErrors(code)
+  }
+
+  test("Generic method with wrong given Generic Type") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericMethodTypeFailure.oinca")
+    checkModuleErrors(code)
+  }
+
+  test("Generic method with wrong given Generic Type (multiple Types)") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/GenericMethodTypeFailure.oinca")
+    checkModuleErrors(code)
+  }
+
+  test("unknown and unbound Type Parameter") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/UnknownTypeParameter.oinca")
+    checkModuleErrors(code)
+  }
+
+  test("unknown and unbound Type Parameter scoping") {
+    val code = FileUtil.readFile("objectoriented/generics/basic/UnknownTypeParamScoping.oinca")
+    checkModuleErrors(code)
+  }
+
+
+  test("Generic Linked List") {
+    val code = FileUtil.readFile("objectoriented/generics/collections/GenericLinkedList.oinca")
+    checkModule(code)
+  }
+
+  test("Generic Linked List (nodes use same type parameter)") {
+    val code = FileUtil.readFile("objectoriented/generics/collections/GenericLinkedListShadowing.oinca")
+    checkModule(code)
+  }
+
+
+  test("Simple generic class inheritance") {
+    val code = FileUtil.readFile("objectoriented/generics/inheritance/GenericClassInheritance.oinca")
+    checkModule(code)
+  }
+
+  test("Generic class inheritance chain") {
+    val code = FileUtil.readFile("objectoriented/generics/inheritance/GenericClassInheritanceChain.oinca")
+    checkModule(code)
+  }
+
+  test("Addition of Generic Param with Inheritance Failure") {
+    val code = FileUtil.readFile("objectoriented/generics/inheritance/GenericClassInheritanceGenericParamAdditionFailure.oinca")
+    checkModuleErrors(code)
+  }
+
+  test("Generic class inheritance method wrong param type Failure") {
+    val code = FileUtil.readFile("objectoriented/generics/inheritance/GenericClassInheritanceMethodTypeFailure.oinca")
+    checkModuleErrors(code)
+  }
+
+  test("Generic class inheritance method with arguments") {
+    val code = FileUtil.readFile("objectoriented/generics/inheritance/GenericClassInheritanceMethodWithArgument.oinca")
+    checkModule(code)
+  }
+
+  test("Generic class inheritance with fixed Type Parameter") {
+    val code = FileUtil.readFile("objectoriented/generics/inheritance/genericClassInheritanceWithFixedTypeParameter.oinca")
+    checkModule(code)
+  }
+
+  test("Generic class inheritance with multiple Type Parameters") {
+    val code = FileUtil.readFile("objectoriented/generics/inheritance/GenericClassInheritanceWithMultipleTypeParameters.oinca")
+    checkModule(code)
+  }
+
+  test("Simple generic method inheritance") {
+    val code = FileUtil.readFile("objectoriented/generics/inheritance/GenericMethodInheritance.oinca")
+    checkModule(code)
+  }
+
+
+  test("Simple nested generics") {
+    val code = FileUtil.readFile("objectoriented/generics/nested/NestedGenerics.oinca")
+    checkModule(code)
+  }
+
+  test("Nested generics") {
+    val code = FileUtil.readFile("objectoriented/generics/nested/NestedGenerics2.oinca")
+    checkModule(code)
+  }
+
+  test("Nested generics with inheritance") {
+    val code = FileUtil.readFile("objectoriented/generics/nested/NestedGenericsInheritance.oinca")
+    checkModule(code)
+  }
+
+
+
+
+
+
 }
