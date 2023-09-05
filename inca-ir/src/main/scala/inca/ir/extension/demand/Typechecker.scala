8f6f228612a61b2878d733762e06b240f0c31bb9
Sebastian Erdweg <seba.github@gmail.com>
2023-09-05 16:38:19 +0200

    adds notes on type checking demand

--- inca-ir/src/main/scala/inca/ir/extension/demand/Typechecker.scala
+++ inca-ir/src/main/scala/inca/ir/extension/demand/Typechecker.scala
@@ -4,6 +4,36 @@ import inca.ir.Atom
 import inca.ir.extension.disjunction.Disjunction
 import inca.ir.typing.BaseIRTypechecker
 
+/*
+R(y) :- {A1(x) or A2(x)}, Q(x, y).
+Q(x, y) :- demand(x), B(x, y), C(x, y).
+
+R(y) :- {A1(x) or A2(x)}, B(x, y), C(x, y).
+
+R(y) :- {A1(x) or A2(x)}, demand(x), B(x, y), C(x, y).
+
+R(y) :- {A1(x) or A2(x)}, Q(x, y).
+Q(x, y) :- B(x, y), C(x, y).
+ */
+
+/*
+fails to compile, because x cannot be bound in R
+R(y) :- Q(x, y).
+Q(x, y) :- demand(x), B(x, y), C(x, y).
+to
+R(y) :- demand(x), Q(x, y).
+Q(x, y) :- inputGuard(x), B(x, y), C(x, y).
+
+
+successfully compiles
+R(y) :- Q(y, 0).
+Q(x, y) :- demand(x), B(x, y), C(x, y).
+to
+R(y) :- demandHere(y), Q(y, 0).
+Q(x, y) :- demandHere(x), B(x, y), C(x, y).
+
+ */
+
 trait Typechecker extends BaseIRTypechecker:
   override def typecheck(atom: Atom): Unit = atom match
     case Demand(ts) =>
