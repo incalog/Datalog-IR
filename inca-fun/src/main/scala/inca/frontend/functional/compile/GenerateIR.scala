fff0810224a1ff68bf2ef60b76c07df4a430d207
David Klopp <davklopp@uni-mainz.de>
2023-10-25 00:53:15 +0100

    Add tests for set and adapt the function frontend to unpack them

--- inca-fun/src/main/scala/inca/frontend/functional/compile/GenerateIR.scala
+++ inca-fun/src/main/scala/inca/frontend/functional/compile/GenerateIR.scala
@@ -54,14 +54,29 @@ class GenerateIR {
 
   def compileMainFun(f: FunctionDef): ir.Relation =
     val result = gensym.fresh(f.name.name + "_result")
-    val resultParam = ir.Param(Name(result), compileType(f.outType))
+    val setMemberResult = gensym.fresh("set_result")
+    // Automatically unpack sets
+    // Note: this might be problematic if we can return sets nested in tuples
+    val returnsSet = f.outType.isInstanceOf[TSet]
+    val outType = f.outType match
+      case TSet(ty) => compileType(ty)
+      case ty => compileType(ty)
+    val resultParam = ir.Param(Name(result), outType)
     val params = f.params.map(p => ir.Param(p.name, compileType(p.typ))) :+ resultParam
+    val edbCall = ir.ExtensionalCall(extensionalRelationName(f.name), f.params.map(p => ir.Var(p.name)))
     ir.Relation(f.name, params, Seq(ir.Body(
-      Seq(
-        ir.ExtensionalCall(extensionalRelationName(f.name), f.params.map(p => ir.Var(p.name))),
-        ir.Eq(ir.Var(Name(result)), compileExp(f.body)))
-      )
-    ))
+      if (returnsSet)
+        Seq(
+          edbCall,
+          irset.SetMember(ir.Var(Name(setMemberResult)), compileExp(f.body)),
+          ir.Eq(ir.Var(Name(result)), ir.Var(Name(setMemberResult)))
+        )
+      else
+        Seq(
+          edbCall,
+          ir.Eq(ir.Var(Name(result)), compileExp(f.body))
+        )
+    )))
 
   def compileFun(f: FunctionDef): ir.Relation =
     val result = gensym.fresh(f.name.name + "_result")
