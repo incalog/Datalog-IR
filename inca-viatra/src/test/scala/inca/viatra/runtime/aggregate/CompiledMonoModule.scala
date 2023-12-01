5140cf90525dfc55330817f8b42c5256c7c36bb8
Runqing Xu <runxu@uni-mainz.de>
2023-12-01 13:08:43 +0100

    Remove old lowering code in mono extension.

--- inca-viatra/src/test/scala/inca/viatra/runtime/aggregate/CompiledMonoModule.scala
+++ inca-viatra/src/test/scala/inca/viatra/runtime/aggregate/CompiledMonoModule.scala
@@ -1,7 +1,7 @@
 package inca.viatra.runtime.aggregate
 
 import inca.ir.extension.*
-import inca.ir.extension.mono.{Lowering, NewLowering}
+import inca.ir.extension.mono.Lowering
 import inca.ir.util.SourceLocation
 import inca.ir.visitors.BaseIRVisitor
 import inca.ir.{CompiledModule, Module, Name}
@@ -15,7 +15,7 @@ case class CompiledMonoModule(mod: Module) extends CompiledModule:
 
 object CompiledMonoModule:
   val pipeline: List[() => BaseIRVisitor] = List(
-    () => new NewLowering {},
+    () => new Lowering {},
     () => new impure.Lowering {},
     () => new aggregateset.Lowering {},
     () => new set.Lowering {},
