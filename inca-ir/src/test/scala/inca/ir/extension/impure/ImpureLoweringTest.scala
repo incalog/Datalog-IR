44c67e1420adf6f779691772cc331c457834b2e2
Runqing Xu <runxu@uni-mainz.de>
2023-12-11 10:57:19 +0100

    Fix failed tests.

--- inca-ir/src/test/scala/inca/ir/extension/impure/ImpureLoweringTest.scala
+++ inca-ir/src/test/scala/inca/ir/extension/impure/ImpureLoweringTest.scala
@@ -84,7 +84,7 @@ class ImpureLoweringTest extends AnyFunSuiteLike:
         )
       )
     )
-    val vars = m.relations("R").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name)
+    val vars = m.relations("R").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name.name)
     assert((1 until 4).forall(i => vars.contains(s"alloc$$$i")))
   }
 
@@ -119,8 +119,8 @@ class ImpureLoweringTest extends AnyFunSuiteLike:
       )
     )
 
-    val rVars = m.relations("R").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name)
-    val qVars = m.relations("Q").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name)
+    val rVars = m.relations("R").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name.name)
+    val qVars = m.relations("Q").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name.name)
     assert((1 until 6).forall(i => rVars.contains(s"alloc$$$i")))
     assert(qVars.contains("alloc$0"))
   }
@@ -161,8 +161,8 @@ class ImpureLoweringTest extends AnyFunSuiteLike:
       )
     )
 
-    val rVars = m.relations("R").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name)
-    val fieldVars = m.relations("Field").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name)
+    val rVars = m.relations("R").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name.name)
+    val fieldVars = m.relations("Field").bodies.flatMap(_.atoms.flatMap(_.vars)).map(_.ref.name.name)
     assert((1 until 8).forall(i => rVars.contains(s"alloc$$$i")))
     assert((1 until 5).forall(i => rVars.contains(s"update$$$i")))
     assert((1 until 1).forall(i => fieldVars.contains(s"alloc$$$i")))
