e7329febd97350c3dcea0571051d5257abdf3d57
David Klopp <davklopp@uni-mainz.de>
2024-01-16 13:53:18 +0100

    Use endNode and step variable in DependencyAnalysis.oodl

--- inca-oodl/src/test/scala/inca/frontend/oodl/executor/casestudies/OODLViatraExecutorCaseStudyTest.scala
+++ inca-oodl/src/test/scala/inca/frontend/oodl/executor/casestudies/OODLViatraExecutorCaseStudyTest.scala
@@ -11,19 +11,22 @@ class OODLViatraExecutorCaseStudyTest extends AnyFunSuite:
   val exec: OODLExecutor = new OODLExecutor(new inca.viatra.Executor)
 
   test("DependencyAnalysis") {
+    val endNode = 50
+    val step = 10
+
     val code = FileUtil.readFileFromResource("objectoriented/casestudies/DependencyAnalysis.oodl")
     val compiled = exec.compileOODL(code, options)
     compiled.setPipeline(CompiledOODLModule.pipeline)
     val loaded = exec.loadOODL(compiled)
 
-    //val start = System.nanoTime()
-    var res = loaded.execute("main", Seq(10, 2))
-    //val diff = System.nanoTime() - start
+    val start = System.nanoTime()
+    var res = loaded.execute("main", Seq(endNode, step))
+    val diff = System.nanoTime() - start
 
     val setAdt = res.entries.head
     val query = Relation.from("Set$TString$enum", Seq("$set"), Seq(Seq(setAdt)))
     res = loaded.engine.read(query).project(1, 2)
 
-    //println(diff.toDouble / 1000 / 1000 / 1000)
-    assertResult(Set("a0", "a1", "a2"))(res.toSet)
+    println(diff.toDouble / 1000 / 1000 / 1000)
+    assertResult(0.to(endNode).map("a" + _).toSet)(res.toSet)
   }
