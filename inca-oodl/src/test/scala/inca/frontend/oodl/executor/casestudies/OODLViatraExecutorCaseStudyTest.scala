bfb87673c9f71d19c144b734ceac2f97d4060338
David Klopp <davklopp@uni-mainz.de>
2024-01-16 11:55:36 +0100

    Minor

--- inca-oodl/src/test/scala/inca/frontend/oodl/executor/casestudies/OODLViatraExecutorCaseStudyTest.scala
+++ inca-oodl/src/test/scala/inca/frontend/oodl/executor/casestudies/OODLViatraExecutorCaseStudyTest.scala
@@ -5,9 +5,7 @@ import inca.frontend.oodl.executor.{OODLExecutor, TypeCastException}
 import inca.ir.execution.Relation
 import inca.util.FileUtil
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.{Ignore, stats}
 
-// TODO: Why is this so, so much slower than the old hacked mono implementation ?
 class OODLViatraExecutorCaseStudyTest extends AnyFunSuite:
   val options = OODLCompilerOptions.fromResource("objectoriented/Options.ini")
   val exec: OODLExecutor = new OODLExecutor(new inca.viatra.Executor)
@@ -18,15 +16,14 @@ class OODLViatraExecutorCaseStudyTest extends AnyFunSuite:
     compiled.setPipeline(CompiledOODLModule.pipeline)
     val loaded = exec.loadOODL(compiled)
 
-    val start = System.nanoTime()
+    //val start = System.nanoTime()
     var res = loaded.execute("main", Seq(10, 2))
-    val diff = System.nanoTime() - start
+    //val diff = System.nanoTime() - start
 
     val setAdt = res.entries.head
     val query = Relation.from("Set$TString$enum", Seq("$set"), Seq(Seq(setAdt)))
     res = loaded.engine.read(query).project(1, 2)
 
-    println(diff.toDouble / 1000 / 1000 / 1000)
-    // TODO: Is this even the correct expected result ?
+    //println(diff.toDouble / 1000 / 1000 / 1000)
     assertResult(Set("a0", "a1", "a2"))(res.toSet)
   }
