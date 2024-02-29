35b1d7b89f51b48e4cde181417ed6679394ef8a5
André Pacak <pacak@uni-mainz.de>
2024-02-29 21:57:06 +0100

    toString for structural uid

--- inca-foreign-scala/src/main/scala/inca/foreign/scala/data/StructuralUID.scala
+++ inca-foreign-scala/src/main/scala/inca/foreign/scala/data/StructuralUID.scala
@@ -1,3 +1,5 @@
 package inca.foreign.scala.data
 
-case class StructuralUID(constr: String, args: Any*)
+case class StructuralUID(constr: String, args: Any*):
+  override def toString: String =
+    s"""${constr}_uid(${args.mkString(", ")})"""
