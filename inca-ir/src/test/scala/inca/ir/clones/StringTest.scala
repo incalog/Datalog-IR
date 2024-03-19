9f9599d85d65a3fbd71136974b22c323de332825
Justus Delerme <jdelerme@students.uni-mainz.de>
2024-03-19 19:26:41 +0100

    added some tests for strings

--- inca-ir/src/test/scala/inca/ir/clones/StringTest.scala
+++ inca-ir/src/test/scala/inca/ir/clones/StringTest.scala
@@ -3,8 +3,7 @@ package inca.ir.clones
 import inca.ir.{BaseIR, Body, Eq, Language, Name, Param, Relation, Var, Module as IRModule}
 import inca.ir.extension.arithmetic.{IntNum, TInt}
 import inca.ir.extension.{arithmetic, string}
-import inca.ir.extension.string.{TString, StringLit}
-
+import inca.ir.extension.string.{StringConcat, StringLit, TString}
 import inca.ir.string2name
 
 
@@ -34,6 +33,88 @@ class StringTest extends ValueNumberingTestAbstract {
         ))
       ))
     performTest(expected,input)
+  }
+
+  test("Simple Redundant concat in Eq") {
+    val input = IRModule(Name("Datalog"), Language(Set(new BaseIR {}, new arithmetic.IR {}, new string.IR {})),
+      Seq(
+        Relation(Name("R"), Seq(Param("param$0", TString), Param("param$1", TString)), Seq(
+          Body(Seq(
+            Eq(Var(Name("A")), StringConcat(StringLit("Hello"), StringLit(" World"))),
+            Eq(Var(Name("B")), StringConcat(StringLit("Hello"), StringLit(" World"))),
+            Eq(Var(Name("param$0")), Var(Name("A"))),
+            Eq(Var(Name("param$1")), Var(Name("B")))
+          ))
+        ))
+      ))
+    val expected = IRModule(Name("Datalog"), Language(Set(new BaseIR {}, new arithmetic.IR {}, new string.IR {})),
+      Seq(
+        Relation(Name("R"), Seq(Param("param$0", TString), Param("param$1", TString)), Seq(
+          Body(Seq(
+            Eq(Var(Name("A")), StringConcat(StringLit("Hello"), StringLit(" World"))),
+//            Eq(Var(Name("B")), StringConcat(StringLit("Hello"), StringLit(" World"))),
+            Eq(Var(Name("param$0")), Var(Name("A"))),
+            Eq(Var(Name("param$1")), Var(Name("A")))
+          ))
+        ))
+      ))
+    performTest(expected, input)
+  }
+
+  test("Redundant concat in Eq") {
+    val input = IRModule(Name("Datalog"), Language(Set(new BaseIR {}, new arithmetic.IR {}, new string.IR {})),
+      Seq(
+        Relation(Name("R"), Seq(Param("param$0", TString), Param("param$1", TString)), Seq(
+          Body(Seq(
+            Eq(Var("H"),StringLit("Hello")),
+            Eq(Var(Name("A")), StringConcat(Var("H"), StringLit(" World"))),
+            Eq(Var(Name("B")), StringConcat(StringLit("Hello"), StringLit(" World"))),
+            Eq(Var(Name("param$0")), Var(Name("A"))),
+            Eq(Var(Name("param$1")), Var(Name("B")))
+          ))
+        ))
+      ))
+    val expected = IRModule(Name("Datalog"), Language(Set(new BaseIR {}, new arithmetic.IR {}, new string.IR {})),
+      Seq(
+        Relation(Name("R"), Seq(Param("param$0", TString), Param("param$1", TString)), Seq(
+          Body(Seq(
+            Eq(Var("H"),StringLit("Hello")),
+            Eq(Var(Name("A")), StringConcat(Var("H"), StringLit(" World"))),
+//            Eq(Var(Name("B")), StringConcat(StringLit("Hello"), StringLit(" World"))),
+            Eq(Var(Name("param$0")), Var(Name("A"))),
+            Eq(Var(Name("param$1")), Var(Name("A")))
+          ))
+        ))
+      ))
+    performTest(expected, input)
+  }
+
+  test("Redundant concat in Eq with different StringLits") {
+    val input = IRModule(Name("Datalog"), Language(Set(new BaseIR {}, new arithmetic.IR {}, new string.IR {})),
+      Seq(
+        Relation(Name("R"), Seq(Param("param$0", TString), Param("param$1", TString)), Seq(
+          Body(Seq(
+            Eq(Var("H"), StringLit("Hello")),
+            Eq(Var(Name("A")), StringConcat(Var("H"), StringLit(" World"))),
+            Eq(Var(Name("B")), StringConcat(StringLit("Hell"), StringLit("o World"))), // need to implement simplify function for this
+            Eq(Var(Name("param$0")), Var(Name("A"))),
+            Eq(Var(Name("param$1")), Var(Name("B")))
+          ))
+        ))
+      ))
+    val expected = IRModule(Name("Datalog"), Language(Set(new BaseIR {}, new arithmetic.IR {}, new string.IR {})),
+      Seq(
+        Relation(Name("R"), Seq(Param("param$0", TString), Param("param$1", TString)), Seq(
+          Body(Seq(
+            Eq(Var("H"), StringLit("Hello")),
+            Eq(Var(Name("A")), StringConcat(Var("H"), StringLit(" World"))),
+            //            Eq(Var(Name("B")), StringConcat(StringLit("Hello"), StringLit(" World"))),
+            Eq(Var(Name("param$0")), Var(Name("A"))),
+            Eq(Var(Name("param$1")), Var(Name("A")))
+          ))
+        ))
+      ))
+    performTest(expected, input)
   }
 
 }
