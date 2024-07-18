c66968d489a9c34c9abd5766b52264c0f98053ac
David Klopp <davklopp@uni-mainz.de>
2024-07-18 16:09:10 +0200

    Compute DNF to produce result from paper

--- inca-ir/src/main/scala/inca/ir/extension/bool/SyntacticOptimizer.scala
+++ inca-ir/src/main/scala/inca/ir/extension/bool/SyntacticOptimizer.scala
@@ -27,16 +27,46 @@ enum Value:
     case _ => Value.Undetermined
 
 
-// optimize by brute fore, McCluskey algorithm is the better way to go
+// Improvement: Use McCluskey algorithm to reduce dnf
 case class BoolTable(t: Term):
-  lazy val results: Map[Map[Name, Value], Value] = {
+  type VariableAssignment = Map[Name, Value]
+
+  lazy val results: Map[VariableAssignment, Value] = {
+    // We could limit the depth here to only optimize up to x variables
     val varAssignment = generateVariableAssignments(t.vars.map(_.name))
     varAssignment.map(as => as -> eval(t, as)).toMap
   }
 
   def uniqueResult: Value = if results.values.toSet.size == 1 then results.values.head else Value.Undetermined
 
-  private def generateVariableAssignments(names: Seq[Name]): Seq[Map[Name, Value]] =
+  def dnf: Option[Term] =
+    this.uniqueResult match
+      case Value.True => Some(BoolTrue)
+      case Value.False => Some(BoolFalse)
+      // we can not create the DNF if any term is undetermined
+      case _ if results.values.toSet.contains(Value.Undetermined) => None
+      case _ =>
+        val conjunctions = results.flatMap {
+          case (varAssignment, Value.True) =>
+            val terms = varAssignment.map {
+              case (varName, Value.True) => Var(varName)
+              case (varName, Value.False) => BoolNot(Var(varName))
+            }
+            val conjunction = terms.fold(BoolTrue) {
+              case (BoolTrue, t) => t
+              case (acc, t) => BoolAnd(acc, t)
+            }
+            Some(conjunction)
+          case _ => None
+        }
+
+        val disjunctions = conjunctions.fold(BoolFalse) {
+          case (BoolFalse, t) => t
+          case (acc, t) => BoolOr(acc, t)
+        }
+        Some(disjunctions)
+
+  private def generateVariableAssignments(names: Seq[Name]): Seq[VariableAssignment] =
     val numNames = names.length
     val binaryCombinations = (0 until math.pow(2, numNames).toInt).map { i =>
       val binaryString = i.toBinaryString.reverse.padTo(numNames, '0').reverse
@@ -44,18 +74,21 @@ case class BoolTable(t: Term):
     }
     binaryCombinations.map { combination => names.zip(combination).toMap }
 
-  private def eval(t: Term, values: Map[Name, Value]): Value = t match
-    case BoolNot(t1) => !eval(t1, values)
-    case BoolOr(t1, t2) => eval(t1, values) || eval(t2, values)
-    case BoolAnd(t1, t2) => eval(t1, values) && eval(t2, values)
+  private def eval(t: Term, variableAssignment: VariableAssignment): Value = t match
+    case BoolNot(t1) => !eval(t1, variableAssignment)
+    case BoolOr(t1, t2) => eval(t1, variableAssignment) || eval(t2, variableAssignment)
+    case BoolAnd(t1, t2) => eval(t1, variableAssignment) && eval(t2, variableAssignment)
     case BoolTrue => Value.True
     case BoolFalse => Value.False
-    case Var(ref) => values(ref.name)
+    case Var(ref) => variableAssignment(ref.name)
     case _ => Value.Undetermined
 
 
 trait SyntacticOptimizer extends IRVisitor:
   override def name: String = "SyntacticBoolOptimizer"
+
+  // term -> DNF if exists or original visited Term
+  var cache: Map[Term, Seq[Term]] = Map()
 
   override def visitTerm(term: Term): Seq[Term] = term match
       case BoolAnd(t1, t2) => eval(term)
@@ -63,9 +96,13 @@ trait SyntacticOptimizer extends IRVisitor:
       case BoolNot(t1) => eval(term)
       case _ => super.visitTerm(term)
 
-  private def eval(t: Term): Seq[Term] =
-    val boolTable = BoolTable(t)
-    boolTable.uniqueResult match
-      case Value.Undetermined => super.visitTerm(t)
-      case Value.True => Seq(BoolTrue)
-      case Value.False => Seq(BoolFalse)
+  private def eval(t: Term): Seq[Term] = cache.get(t) match
+    case Some(cachedDnf) => cachedDnf
+    case _ =>
+      val boolTable = BoolTable(t)
+      val dnf = boolTable.dnf match
+        case Some(term) => Seq(term)
+        // we use an unsupported feature, such as blocks inside a boolean
+        case _ => super.visitTerm(t)
+      cache += t -> dnf
+      dnf
