073fe623a491b6eb44c04ef94cc835d1e1e78551
David Klopp <davklopp@uni-mainz.de>
2025-02-19 14:18:28 +0100

    Refine RemoveDuplicatedRelations to be more aggressive

--- inca-fun/src/main/scala/inca/frontend/functional/compile/CompiledFunctionalUnit.scala
+++ inca-fun/src/main/scala/inca/frontend/functional/compile/CompiledFunctionalUnit.scala
@@ -146,5 +146,6 @@ object CompiledFunctionalUnit:
     () => new optimize.IRConstantOptimizer(assumeEdbIsNotEmpty = true, computeControlEvents = false, interRelational = true) {},
     () => new optimize.IdentityCastElimination {},
     () => new optimize.AliasElimination {},
-    //() => new optimize.WildcardDetection {}
+    () => new optimize.WildcardDetection {}, // helps with detecting exact duplicates
+    () => new optimize.RemoveDuplicatedRelations {},
   )
--- inca-ir/src/main/scala/inca/ir/optimize/RemoveDuplicatedRelations.scala
+++ inca-ir/src/main/scala/inca/ir/optimize/RemoveDuplicatedRelations.scala
@@ -4,11 +4,12 @@ import inca.ir.*
 import inca.ir.Hint.preserveHints
 import inca.ir.extension.aggregate.Aggregate
 import inca.ir.extension.aggregateset.AggregateSet
-import inca.ir.typing.Mode
 import inca.ir.visitors.IRVisitor
+import scala.compiletime.uninitialized
 
 /**
- * Remove duplicated relations that contain the exact same bodies.
+ * Remove duplicated relations that contain the exact same bodies. It also removes bodies of the same relation, that are
+ * exactly the same.
  * The demand LoweringWithSupplementaries, might produce multiple relations with different parameters,
  * but the exact same bodies.
  * This optimization will detect these colliding relations, determine the minimum set of parameters,
@@ -18,44 +19,59 @@ trait RemoveDuplicatedRelations extends IRVisitor, Optimizer:
   override def name: String = "RemoveDuplicatedRelations"
 
   // Detect relations with the exact same body
-  var collisionMap: Map[Set[Body], Seq[Relation]] = Map()
+  var collisionMap: Map[Set[Body], Seq[Relation]] = uninitialized
 
   // Original relation name -> (new relation name, mapping from old to new params)
-  var rewritingMap: Map[Name, (Name, Seq[Int])] = Map()
+  var rewritingMap: Map[Name, (Name, Seq[Int])] = uninitialized
 
   enum Phase:
     case CollectCollisions
     case RewriteCalls
 
-  private var phase: Phase = _
+  private var phase: Phase = uninitialized
 
   override def visitModule(module: Module): Module = preserveHints(module) {
-    // Detect all colliding relations and remove them all
-    phase = Phase.CollectCollisions
-    val Module(name, lang, contents) = super.visitModule(module)
+    var changed = true
+    var optimized = module
 
-    // Insert one relation for each collision
-    val newRelations = collisionMap.map {
-      case (_, Seq(rel)) => rel
-      case (bodies, rels) =>
-        val newRelName = rels.head.name
-        val allParams = rels.flatMap(_.params).distinct
-        rewritingMap ++= rels.map { rel =>
-          val paramReordering = allParams.map(rel.params.indexOf)
-          rel.name -> (newRelName, paramReordering)
-        }.toMap
-        Relation(newRelName, allParams, bodies.toSeq)
+    // Run in a fixpoint until the result is stable
+    while (changed) {
+      collisionMap = Map()
+      rewritingMap = Map()
+
+      // Detect all colliding relations and remove them all
+      phase = Phase.CollectCollisions
+      val Module(name, lang, contents) = super.visitModule(optimized)
+
+      // Insert one relation for each collision
+      val newRelations = collisionMap.map {
+        case (bodies, Seq(rel)) =>
+          Relation(rel.name, rel.params, bodies.toSeq)
+        case (bodies, rels) =>
+          val newRelName = rels.head.name
+          val allParams = rels.flatMap(_.params).distinct
+          rewritingMap ++= rels.map { rel =>
+            val paramReordering = allParams.map(rel.params.indexOf)
+            rel.name -> (newRelName, paramReordering)
+          }.toMap
+          Relation(newRelName, allParams, bodies.toSeq)
+      }
+
+      // Rewrite all calls, aggregate calls etc.
+      val mod = Module(name, lang, contents ++ newRelations)
+      phase = Phase.RewriteCalls
+      val newMod = super.visitModule(mod)
+      changed = optimized != newMod
+      optimized = newMod
     }
 
-    // Rewrite all calls, aggregate calls etc.
-    val mod = Module(name, lang, contents ++ newRelations)
-    phase = Phase.RewriteCalls
-    super.visitModule(mod)
+    optimized
   }
 
   override def visitRelation(relation: Relation): Seq[Relation] = phase match
     case Phase.CollectCollisions =>
       val bodies = relation.bodies.toSet
+      println(s"Get the bodies: \n ${bodies.mkString("\n\n")}")
       collisionMap.get(bodies) match
         case None => collisionMap += bodies -> Seq(relation)
         case Some(rels) =>
